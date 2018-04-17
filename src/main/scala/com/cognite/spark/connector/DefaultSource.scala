3ea9db0bbaeb52f2f4b18da7f9d7b05d42378e65
Joel Wilsson <joel.wilsson@cognite.com>
2018-04-17 17:01:13 +0200

    Add read support for raw storage

--- src/main/scala/com/cognite/spark/connector/DefaultSource.scala
+++ src/main/scala/com/cognite/spark/connector/DefaultSource.scala
@@ -17,15 +17,23 @@ class DefaultSource extends RelationProvider
   override def createRelation(sqlContext: SQLContext, parameters: Map[String, String], schema: StructType): BaseRelation = {
     val apiKey = parameters.getOrElse("apiKey", sys.error("ApiKey must be specified."))
     val project = parameters.getOrElse("project", sys.error("Project must be specified"))
-    val path = parameters.getOrElse("path", sys.error("TagId must be specified (as a parameter to load())"))
+    val resourceType = parameters.getOrElse("type", sys.error("Resource type must be specified"))
     val batchSize = parameters.getOrElse("batchsize", "1000").toInt
-    new FullScanRelations(apiKey = apiKey,
-      project = project,
-      path = path,
-      suppliedSchema = schema,
-      batchSize = batchSize,
-      start = parameters.get("start").map(v => v.toLong).orElse(None),
-      stop = parameters.get("stop").map(v => v.toLong).orElse(None)
-    )(sqlContext)
+    val path = parameters.getOrElse("path", sys.error("path (tagId or table name) must be specified (as a parameter to load())"))
+    resourceType match {
+      case "timeseries" =>
+        new FullScanRelations(apiKey = apiKey,
+          project = project,
+          path = path,
+          suppliedSchema = schema,
+          batchSize = batchSize,
+          start = parameters.get("start").map(v => v.toLong).orElse(None),
+          stop = parameters.get("stop").map(v => v.toLong).orElse(None)
+        )(sqlContext)
+      case "tables" =>
+        val project = parameters.getOrElse("project", sys.error("Project must be specified"))
+        val database = parameters.getOrElse("database", sys.error("Database must be specified"))
+        new RawTableRelation(apiKey, project, database, path, None, None)(sqlContext)
+    }
   }
 }
