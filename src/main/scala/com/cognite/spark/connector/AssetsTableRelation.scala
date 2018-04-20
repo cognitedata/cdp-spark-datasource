3501190d28b63b1c14edbc953c0625513460690c
Joel Wilsson <joel.wilsson@cognite.com>
2018-04-20 16:17:15 +0200

    Add support for assets

---
+++ src/main/scala/com/cognite/spark/connector/AssetsTableRelation.scala
@@ -0,0 +1,186 @@
+package com.cognite.spark.connector
+
+import java.util.concurrent.TimeUnit
+
+import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import okhttp3._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.sources.{BaseRelation, InsertableRelation, TableScan}
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
+import org.apache.spark.sql.{Row, SQLContext}
+
+import scala.collection.mutable.ListBuffer
+
+case class AssetsData(data: AssetsDataItems[AssetsItem])
+
+// TODO: move this to a shared place, name them DataItemsWithCursor and DataItems
+case class AssetsDataItems[A](items: Seq[A], nextCursor: Option[String] = None)
+
+case class PostAssetsDataItems[A](items: Seq[A])
+
+case class AssetsItem(name: String,
+                      // FIXME: parentId is optional
+                      parentId: Long,
+                      description: String,
+                      metadata: Map[String, String],
+                      id: Long)
+
+case class PostAssetsItem(name: String,
+                          description: String,
+                          metadata: Map[String, String])
+
+// TODO: there's obviously a lot of copy-paste going on here at the moment,
+// and this should be refactored once we've identified some common patterns.
+class AssetsTableRelation(apiKey: String,
+                          project: String,
+                          assetPath: Option[String],
+                          limit: Option[Long],
+                          batchSizeOption: Option[Int])(@transient val sqlContext: SQLContext)
+  extends BaseRelation
+    with InsertableRelation
+    with TableScan
+    // TODO: we shouldn't need to serialize this class, need to fix some transients etc. to avoid it
+    with Serializable {
+  val DEFAULT_BATCH_SIZE = 10000
+
+  // TODO: make read/write timeouts configurable
+  @transient lazy val client: OkHttpClient = new OkHttpClient.Builder()
+    .readTimeout(2, TimeUnit.MINUTES)
+    .writeTimeout(2, TimeUnit.MINUTES)
+    .build()
+  @transient lazy val mapper: ObjectMapper = {
+    val mapper = new ObjectMapper()
+    mapper.registerModule(DefaultScalaModule)
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    mapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false)
+    mapper
+  }
+  val batchSize = batchSizeOption match {
+    case Some(size) => size
+    case None => DEFAULT_BATCH_SIZE
+  }
+
+  override def schema: StructType = StructType(Seq(
+    StructField("name", DataTypes.StringType),
+    StructField("parentId", DataTypes.LongType),
+    StructField("description", DataTypes.StringType),
+    StructField("metadata", DataTypes.createMapType(DataTypes.StringType, DataTypes.StringType)),
+    StructField("id", DataTypes.LongType)))
+
+  override def buildScan(): RDD[Row] = {
+    val responses: ListBuffer[Row] = ListBuffer()
+    var doneReading = false
+    var cursor: Option[String] = None
+    var nRowsRemaining: Option[Long] = limit
+
+    do {
+      val thisBatchSize = scala.math.min(nRowsRemaining.getOrElse(batchSize.toLong).toInt, batchSize)
+      val urlBuilder = AssetsTableRelation.baseAssetsURL(project)
+        .addQueryParameter("limit", thisBatchSize.toString)
+      if (!assetPath.isEmpty) {
+        urlBuilder.addQueryParameter("path", assetPath.get)
+      }
+      if (!cursor.isEmpty) {
+        urlBuilder.addQueryParameter("cursor", cursor.get)
+      }
+
+      var response: Response = null
+      try {
+        println("querying " + AssetsTableRelation.baseRequest(apiKey)
+          .url(urlBuilder.build())
+          .build())
+        response = client.newCall(
+          AssetsTableRelation.baseRequest(apiKey)
+            .url(urlBuilder.build())
+            .build())
+          .execute()
+        if (!response.isSuccessful) {
+          throw new RuntimeException("Non-200 status when querying API, received " + response.code() + "(" + response.message() + ")")
+        }
+        val r = mapper.readValue(response.body().string(), classOf[AssetsData])
+        for (item <- r.data.items) {
+          responses += Row(item.name, item.parentId, item.description, item.metadata, item.id)
+        }
+
+        cursor = r.data.nextCursor
+        nRowsRemaining = nRowsRemaining.map(_ - r.data.items.size)
+      } finally {
+        if (response != null) {
+          response.close()
+        }
+      }
+
+    } while (!cursor.isEmpty && (nRowsRemaining.isEmpty || nRowsRemaining.get > 0))
+
+    sqlContext.sparkContext.parallelize(responses)
+  }
+
+  override def insert(df: org.apache.spark.sql.DataFrame, overwrite: scala.Boolean): scala.Unit = {
+    df.foreachPartition(rows => {
+      rows.grouped(batchSize).foreach(postRows)
+    })
+  }
+
+  private def postRows(rows: Seq[Row]) = {
+    // this definitely seems like a common pattern that could be refactored out to a common method
+    val assetItems = rows.map(r =>
+      PostAssetsItem(r.getString(0), r.getString(2), r.getAs[Map[String, String]](3)))
+    val items = new PostAssetsDataItems[PostAssetsItem](assetItems)
+
+    val jsonMediaType = MediaType.parse("application/json; charset=utf-8")
+    val requestBody = RequestBody.create(jsonMediaType, mapper.writeValueAsString(items))
+    println("Posting '" + mapper.writeValueAsString(items) + "' to " + AssetsTableRelation.baseAssetsURL(project).build().toString)
+    val response = client.newCall(
+      RawTableRelation.baseRequest(apiKey)
+        .url(AssetsTableRelation.baseAssetsURL(project).build())
+        .post(requestBody)
+        .build()
+    ).execute()
+    if (!response.isSuccessful) {
+      throw new RuntimeException("Non-200 status when posting to raw API, received " + response.code() + "(" + response.message() + ")")
+    }
+  }
+}
+
+object AssetsTableRelation {
+  val mapper: ObjectMapper = {
+    val mapper = new ObjectMapper()
+    mapper.registerModule(DefaultScalaModule)
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    mapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false)
+    mapper
+  }
+
+  def baseAssetsURL(project: String): HttpUrl.Builder = {
+    new HttpUrl.Builder()
+      .scheme("https")
+      .host("api.cognitedata.com")
+      .addPathSegments("api/0.4/projects")
+      .addPathSegment(project)
+      .addPathSegment("assets")
+  }
+
+  def baseRequest(apiKey: String): Request.Builder = {
+    new Request.Builder()
+      .header("Content-Type", "application/json")
+      .header("Accept", "application/json")
+      .header("Accept-Charset", "utf-8")
+      .header("api-key", apiKey)
+  }
+
+  val validPathComponentTypes = Seq(
+    classOf[java.lang.Integer],
+    classOf[java.lang.Long],
+    classOf[java.lang.String])
+
+  def isValidAssetsPath(path: String): Boolean = {
+    try {
+      val pathComponents = mapper.readValue(path, classOf[Seq[Any]])
+      pathComponents.forall(c => validPathComponentTypes.contains(c.getClass))
+    } catch {
+      case _: JsonParseException => false
+    }
+  }
+}
--- src/main/scala/com/cognite/spark/connector/DefaultSource.scala
+++ src/main/scala/com/cognite/spark/connector/DefaultSource.scala
@@ -1,7 +1,5 @@
 package com.cognite.spark.connector
 
-import java.lang.NumberFormatException
-
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.sources.{BaseRelation, DataSourceRegister, RelationProvider, SchemaRelationProvider}
 import org.apache.spark.sql.types.StructType
@@ -20,12 +18,22 @@ class DefaultSource extends RelationProvider
     val apiKey = parameters.getOrElse("apiKey", sys.error("ApiKey must be specified."))
     val project = parameters.getOrElse("project", sys.error("Project must be specified"))
     val resourceType = parameters.getOrElse("type", sys.error("Resource type must be specified"))
-    val path = parameters.getOrElse("path", sys.error("path (tagId or table name) must be specified (as a parameter to load())"))
+    val batchSize = try {
+      parameters.get("batchSize").map(_.toInt)
+    } catch {
+      case _: NumberFormatException => None
+    }
+    val limit = try {
+      parameters.get("limit").map(_.toLong)
+    } catch {
+      case _: NumberFormatException => None
+    }
     resourceType match {
       case "timeseries" =>
+        val tagId = parameters.getOrElse("path", sys.error("tagId must be specified (as a parameter to load())"))
         new FullScanRelations(apiKey = apiKey,
           project = project,
-          path = path,
+          path = tagId,
           suppliedSchema = schema,
           batchSize = parameters.getOrElse("batchSize", "1000").toInt,
           start = parameters.get("start").map(v => v.toLong).orElse(None),
@@ -33,20 +41,18 @@ class DefaultSource extends RelationProvider
         )(sqlContext)
       case "tables" =>
         val database = parameters.getOrElse("database", sys.error("Database must be specified"))
-        val batchSize = try {
-          parameters.get("batchSize").map(_.toInt)
-        } catch {
-          case _: NumberFormatException => None
-        }
-        val limit = try {
-          parameters.get("limit").map(_.toLong)
-        } catch {
-          case _: NumberFormatException => None
-        }
-        new RawTableRelation(apiKey, project, database, path,
+        val tableName = parameters.getOrElse("path", sys.error("table name must be specified (as a parameter to load())"))
+        new RawTableRelation(apiKey, project, database, tableName,
           Option(schema),
           limit,
           batchSize)(sqlContext)
+      case "assets" =>
+        val assetsPath = parameters.get("path")
+        if (assetsPath.isDefined && !AssetsTableRelation.isValidAssetsPath(assetsPath.get)) {
+          sys.error("Invalid assets path: " + assetsPath.get)
+        }
+        new AssetsTableRelation(apiKey, project, assetsPath, limit, batchSize)(sqlContext)
+      case _ => sys.error("Unknown resource type: " + resourceType)
     }
   }
 }
