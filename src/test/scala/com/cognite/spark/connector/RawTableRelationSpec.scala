5b4f361732b3948cbf7f41ce673e935e2234d090
Joel Wilsson <joel.wilsson@gmail.com>
2018-10-11 11:50:04 +0200

    Add tests for key column renaming

---
+++ src/test/scala/com/cognite/spark/connector/RawTableRelationSpec.scala
@@ -0,0 +1,61 @@
+package com.cognite.spark.connector
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types._
+import org.scalatest.{FlatSpec, Matchers}
+
+class RawTableRelationSpec extends FlatSpec with Matchers with DataFrameSuiteBase {
+  import RawTableRelation._
+
+  private def collectToSet(df: DataFrame): Set[String] = {
+    df.collect().map(_.getString(0)).toSet
+  }
+
+  "A RawTableRelation" should "allow data columns named key, _key etc. but rename them to _key, __key etc." in {
+    import spark.implicits._
+
+    val dfWithoutKeySchema = StructType(Seq(
+      StructField("notKey", StringType, false),
+      StructField("value", IntegerType, false)))
+    val dfWithoutKey = Seq(
+      ("key1", """{ "notKey": "k1", "value": 1 }"""),
+      ("key2", """{ "notKey": "k2", "value": 2 }""")
+    ).toDF("key", "columns")
+
+    val dfWithKeySchema = StructType(Seq(
+      StructField("key", StringType, false),
+      StructField("value", IntegerType, false)))
+    val dfWithKey = Seq(
+      ("key3", """{ "key": "k1", "value": 1 }"""),
+      ("key4", """{ "key": "k2", "value": 2 }""")
+    ).toDF("key", "columns")
+
+    val dfWithManyKeysSchema = StructType(Seq(
+      StructField("key", StringType, true),
+      StructField("__key", StringType, false),
+      StructField("___key", StringType, false),
+      StructField("value", IntegerType, false)))
+    val dfWithManyKeys = Seq(
+      ("key5", """{ "___key": "___k1", "__key": "__k1", "value": 1 }"""),
+      ("key6", """{ "___key": "___k2", "value": 2, "__key": "__k2", "key": "k2" }""")
+    ).toDF("key", "columns")
+
+    val processedWithoutKey = flattenAndRenameKeyColumns(sqlContext, dfWithoutKey, dfWithoutKeySchema)
+    processedWithoutKey.schema.fieldNames.toSet should equal (Set("key", "notKey", "value"))
+    collectToSet(processedWithoutKey.select($"key")) should equal (Set("key1", "key2"))
+
+    val processedWithKey = flattenAndRenameKeyColumns(sqlContext, dfWithKey, dfWithKeySchema)
+    processedWithKey.schema.fieldNames.toSet should equal (Set("key", "_key", "value"))
+    collectToSet(processedWithKey.select($"key")) should equal (Set("key3", "key4"))
+    collectToSet(processedWithKey.select($"_key")) should equal (Set("k1", "k2"))
+
+    val processedWithManyKeys = flattenAndRenameKeyColumns(sqlContext, dfWithManyKeys, dfWithManyKeysSchema)
+    processedWithManyKeys.schema.fieldNames.toSet should equal (Set("key", "____key", "___key", "_key", "value"))
+
+    collectToSet(processedWithManyKeys.select($"key")) should equal (Set("key5", "key6"))
+    collectToSet(processedWithManyKeys.select($"_key")) should equal (Set(null, "k2"))
+    collectToSet(processedWithManyKeys.select($"___key")) should equal (Set("__k1", "__k2"))
+    collectToSet(processedWithManyKeys.select($"____key")) should equal (Set("___k1", "___k2"))
+  }
+}
