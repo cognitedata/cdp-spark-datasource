a238450078e49cf60db94509e111363641c75f11
Joel Wilsson <joel.wilsson@gmail.com>
2018-10-11 11:50:04 +0200

    Add tests for key column renaming

--- src/main/scala/com/cognite/spark/connector/RawTableRelation.scala
+++ src/main/scala/com/cognite/spark/connector/RawTableRelation.scala
@@ -29,6 +29,8 @@ class RawTableRelation(apiKey: String,
     with InsertableRelation
     with TableScan
     with Serializable {
+  import RawTableRelation._
+
   // TODO: make read/write timeouts configurable
   @transient lazy val batchSize = batchSizeOption.getOrElse(10000)
   @transient lazy val defaultSchema = StructType(Seq(
@@ -47,6 +49,74 @@ class RawTableRelation(apiKey: String,
   lazy private val rowsCreated = UserMetricsSystem.counter(s"${metricsPrefix}raw.$database.$table.rows.created")
   lazy private val rowsRead = UserMetricsSystem.counter(s"${metricsPrefix}raw.$database.$table.rows.read")
 
+  override val schema: StructType = userSchema.getOrElse {
+    if (inferSchema) {
+      val rdd = readRows(inferSchemaLimit)
+
+      import sqlContext.sparkSession.implicits._
+      val df = sqlContext.createDataFrame(rdd, defaultSchema)
+      val jsonDf = renameKeyColumns(sqlContext.sparkSession.read.json(df.select($"columns").as[String]))
+      StructType(StructField("key", DataTypes.StringType) +: jsonDf.schema.fields)
+    } else {
+      defaultSchema
+    }
+  }
+
+  private def countItems(items: CdpConnector.DataItemsWithCursor[_]): Unit = {
+    rowsRead.inc(items.data.items.length)
+  }
+
+  private def readRows(limit: Option[Int]): RDD[Row] = {
+    val url = baseRawTableURL(project, database, table).build()
+    val result = CdpConnector.get[RawItem](apiKey, url, batchSize, limit,
+      batchCompletedCallback = if (collectMetrics) Some(countItems) else None)
+      .map(item => Row(item.key, item.columns.asJson.noSpaces))
+    sqlContext.sparkContext.parallelize(result.toStream)
+  }
+
+  override def buildScan(): RDD[Row] = {
+    val rdd = readRows(limit)
+    if (schema == defaultSchema || schema == null || schema.tail.isEmpty) {
+      rdd
+    } else {
+      val jsonFieldsSchema = schemaWithoutRenamedKeyColumns(StructType.apply(schema.tail))
+      val df = sqlContext.sparkSession.createDataFrame(rdd, defaultSchema)
+      flattenAndRenameKeyColumns(sqlContext, df, jsonFieldsSchema).rdd
+    }
+  }
+
+  override def insert(df: org.apache.spark.sql.DataFrame, overwrite: scala.Boolean): scala.Unit = {
+    if (!df.columns.contains("key")) {
+      throw new IllegalArgumentException("The dataframe used for insertion must have a \"key\" column")
+    }
+
+    val dfWithKeyRenamed = df.withColumnRenamed("key", temporaryKeyName)
+    val dfWithUnRenamedKeyColumns = unRenameKeyColumns(dfWithKeyRenamed)
+    val columnNames = dfWithUnRenamedKeyColumns.columns.filter(!_.equals(temporaryKeyName))
+    dfWithUnRenamedKeyColumns.foreachPartition(rows => {
+      rows.grouped(batchSize).foreach(postRows(columnNames, _))
+    })
+  }
+
+  private def postRows(nonKeyColumnNames: Array[String], rows: Seq[Row]) = {
+    val items = rows.map(row => RawItem(
+      row.getString(row.fieldIndex(temporaryKeyName)),
+      io.circe.parser.decode[JsonObject](
+        mapper.writeValueAsString(row.getValuesMap[Any](nonKeyColumnNames))).right.get
+      ))
+
+    val url = baseRawTableURL(project, database, table)
+      .addPathSegment("create")
+      .build()
+
+    CdpConnector.post(apiKey, url, items)
+    if (collectMetrics) {
+      rowsCreated.inc(items.length)
+    }
+  }
+}
+
+object RawTableRelation {
   private val keyColumnPattern = """^_*key$""".r
 
   private def keyColumns(schema: StructType): Array[String] = {
@@ -79,86 +149,22 @@ class RawTableRelation(apiKey: String,
     }
   }
 
-  override val schema: StructType = userSchema.getOrElse {
-    if (inferSchema) {
-      val rdd = readRows(inferSchemaLimit)
-
-      import sqlContext.sparkSession.implicits._
-      val df = sqlContext.createDataFrame(rdd, defaultSchema)
-      val jsonDf = renameKeyColumns(sqlContext.sparkSession.read.json(df.select($"columns").as[String]))
-      StructType(StructField("key", DataTypes.StringType) +: jsonDf.schema.fields)
-    } else {
-      defaultSchema
-    }
-  }
-
-  private def countItems(items: CdpConnector.DataItemsWithCursor[_]): Unit = {
-    rowsRead.inc(items.data.items.length)
-  }
-
-  private def readRows(limit: Option[Int]): RDD[Row] = {
-    val url = RawTableRelation.baseRawTableURL(project, database, table).build()
-    val result = CdpConnector.get[RawItem](apiKey, url, batchSize, limit,
-      batchCompletedCallback = if (collectMetrics) Some(countItems) else None)
-      .map(item => Row(item.key, item.columns.asJson.noSpaces))
-    sqlContext.sparkContext.parallelize(result.toStream)
-  }
-
   private val temporaryKeyName = s"TrE85tFQPCb2fEUZ"
 
-  override def buildScan(): RDD[Row] = {
-    val rdd = readRows(limit)
-    if (schema == defaultSchema || schema == null || schema.tail.isEmpty) {
-      rdd
+  def flattenAndRenameKeyColumns(sqlContext: SQLContext, df: DataFrame, jsonFieldsSchema: StructType): DataFrame = {
+    import sqlContext.implicits.StringToColumn
+    import org.apache.spark.sql.functions.from_json
+    val dfWithSchema = df.select($"key", from_json($"columns", jsonFieldsSchema).alias("columns"))
+
+    if (keyColumns(jsonFieldsSchema).isEmpty) {
+      dfWithSchema.select("key", "columns.*")
     } else {
-      val jsonFields = schemaWithoutRenamedKeyColumns(StructType.apply(schema.tail))
-      val df = sqlContext.sparkSession.createDataFrame(rdd, defaultSchema)
-      import sqlContext.implicits.StringToColumn
-      import org.apache.spark.sql.functions.from_json
-      val dfWithSchema = df.select($"key", from_json($"columns", jsonFields).alias("columns"))
-      val flatDf = if (keyColumns(jsonFields).isEmpty) {
-        dfWithSchema.select("key", "columns.*")
-      } else {
-        val dfWithKeyRenamed = dfWithSchema.withColumnRenamed("key", temporaryKeyName)
-        val temporaryFlatDf = renameKeyColumns(dfWithKeyRenamed.select(temporaryKeyName, "columns.*"))
-        temporaryFlatDf.withColumnRenamed(temporaryKeyName, "key")
-      }
-      flatDf.rdd
+      val dfWithKeyRenamed = dfWithSchema.withColumnRenamed("key", temporaryKeyName)
+      val temporaryFlatDf = renameKeyColumns(dfWithKeyRenamed.select(temporaryKeyName, "columns.*"))
+      temporaryFlatDf.withColumnRenamed(temporaryKeyName, "key")
     }
   }
 
-  override def insert(df: org.apache.spark.sql.DataFrame, overwrite: scala.Boolean): scala.Unit = {
-    if (!df.columns.contains("key")) {
-      throw new IllegalArgumentException("The dataframe used for insertion must have a \"key\" column")
-    }
-
-    val dfWithKeyRenamed = df.withColumnRenamed("key", temporaryKeyName)
-    val dfWithUnRenamedKeyColumns = unRenameKeyColumns(dfWithKeyRenamed)
-    val columnNames = dfWithUnRenamedKeyColumns.columns.filter(!_.equals(temporaryKeyName))
-    dfWithUnRenamedKeyColumns.foreachPartition(rows => {
-      rows.grouped(batchSize).foreach(postRows(columnNames, _))
-    })
-  }
-
-  private def postRows(nonKeyColumnNames: Array[String], rows: Seq[Row]) = {
-    val items = rows.map(row => RawItem(
-      row.getString(row.fieldIndex(temporaryKeyName)),
-      io.circe.parser.decode[JsonObject](
-        mapper.writeValueAsString(row.getValuesMap[Any](nonKeyColumnNames))).right.get
-      ))
-
-    val url = RawTableRelation.baseRawTableURL(project, database, table)
-      .addPathSegment("create")
-      .build()
-
-    CdpConnector.post(apiKey, url, items)
-    if (collectMetrics) {
-      rowsCreated.inc(items.length)
-    }
-  }
-}
-
-object RawTableRelation {
   def baseRawTableURL(project: String, database: String, table: String): HttpUrl.Builder = {
     CdpConnector.baseUrl(project, "0.4")
       .addPathSegment("raw")
