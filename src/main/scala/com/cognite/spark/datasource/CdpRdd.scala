5b8bc948bee7de7c867417f8eed480658889d9d8
Joel Wilsson <joel.wilsson@gmail.com>
2018-11-29 20:42:55 +0100

    Fix ordering of CdpRdd.getPartitions

--- src/main/scala/com/cognite/spark/datasource/CdpRdd.scala
+++ src/main/scala/com/cognite/spark/datasource/CdpRdd.scala
@@ -1,37 +1,35 @@
 package com.cognite.spark.datasource
 
 import io.circe.generic.auto._
-import org.apache.spark.{Partition, SparkContext, TaskContext}
+import org.apache.spark._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
 
 case class CdpRddPartition(cursor: String, index: Int) extends Partition
 
-class CdpRdd(sparkContext: SparkContext, apiKey: String, project: String, batchSize: Int, limit: Option[Int]) extends RDD[Row](sparkContext, Nil) {
+class CdpRdd(sparkContext: SparkContext, apiKey: String, project: String, batchSize: Int, limit: Option[Int])
+  extends RDD[Row](sparkContext, Nil) {
   override def getPartitions: Array[Partition] = {
     val url = EventsRelation.baseEventsURL(project).addQueryParameter("onlyCursors", "true").build()
     val parts = CdpConnector.getWithCursor[EventItem](apiKey, url, batchSize, limit)
       .filter(_.cursor.isDefined)
       .map(chunk => chunk.cursor.get)
+    scala.util.Random.shuffle(parts)
+      .toIndexedSeq
       .zipWithIndex
-      .map { case (cursor, index) => CdpRddPartition(cursor, index).asInstanceOf[Partition] }
-
-    println(s"Read ${parts.size} partitions")
-    scala.util.Random.shuffle(parts).toArray
+      .map { case (cursor, index) => CdpRddPartition(cursor, index) }
+      .toArray
   }
 
   override def compute(_split: Partition, context: TaskContext): Iterator[Row] = {
+    println("COMPUTE")
     val split = _split.asInstanceOf[CdpRddPartition]
-    CdpConnector.get[EventItem](apiKey, EventsRelation.baseEventsURL(project)
-      .addQueryParameter("cursor", split.cursor).build(), batchSize, Some(batchSize))
+    val cdpRows = CdpConnector.get[EventItem](apiKey, EventsRelation.baseEventsURL(project).build(), batchSize, Some(batchSize))
       .map(item => {
-//        eventsReceived += 1
         Row(item.id, item.startTime, item.endTime, item.description, item.`type`, item.subtype,
           item.metadata, item.assetIds, item.source, item.sourceId)
       })
-//    val close = () => { }
-//
-//    CompletionIterator[Row, Iterator[Row]](
-//      new InterruptibleIterator(context, cdpRows), close())
+
+    new InterruptibleIterator(context, cdpRows)
   }
 }
