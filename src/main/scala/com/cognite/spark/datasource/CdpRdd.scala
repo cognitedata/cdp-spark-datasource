1f41aa362195200023009f5054d02eabeca7463a
Joel Wilsson <joel.wilsson@gmail.com>
2018-12-03 01:22:36 +0100

    Add custom iterator

--- src/main/scala/com/cognite/spark/datasource/CdpRdd.scala
+++ src/main/scala/com/cognite/spark/datasource/CdpRdd.scala
@@ -1,32 +1,80 @@
 package com.cognite.spark.datasource
 
+import cats.effect.IO
+import com.cognite.spark.datasource.CdpConnector.{DataItemsWithCursor, retryWithBackoff}
 import io.circe.generic.auto._
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
+import org.http4s.Uri
+import com.softwaremill.sttp._
+import com.softwaremill.sttp.circe._
+import com.softwaremill.sttp.asynchttpclient.cats._
 
-case class CdpRddPartition(cursor: String, index: Int) extends Partition
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+case class CdpRddPartition(cursor: Option[String], size: Option[Int], index: Int) extends Partition
 
 class CdpRdd(sparkContext: SparkContext, apiKey: String, project: String, batchSize: Int, limit: Option[Int])
   extends RDD[Row](sparkContext, Nil) {
+
+  implicit val timer = cats.effect.IO.timer(ExecutionContext.global)
+  implicit val sttpBackend = AsyncHttpClientCatsBackend[IO]()
+  private val maxRetries = 10
+
+  private def cursors(url: Uri): Iterator[(Option[String], Option[Int])] = {
+    new Iterator[(Option[String], Option[Int])] {
+      private var nextCursor = Option.empty[String]
+      private var isFirst = true
+      override def hasNext: Boolean = {
+        if (isFirst) {
+          isFirst = false
+          true
+        } else {
+          nextCursor.isDefined
+        }
+      }
+
+      override def next(): (Option[String], Option[Int]) = {
+        val next = nextCursor
+        val getUrl = nextCursor.fold(url)(cursor => url.withQueryParam("cursor", cursor))
+        val result = sttp.header("api-key", apiKey).get(uri"${getUrl.toString()}").response(asJson[DataItemsWithCursor[EventItem]])
+          .parseResponseIf(_ => true)
+          .send()
+          .map(r => r.unsafeBody match {
+            case Left(error) => throw new RuntimeException(s"boom ${error.message}, ${r.statusText} ${r.code.toString} ${r.toString()}")
+            case Right(items) => items
+          })
+        val dataWithCursor = retryWithBackoff(result, 30.millis, maxRetries)
+          .unsafeRunSync()
+          .data
+        nextCursor = dataWithCursor.nextCursor
+        (next, nextCursor.map(_ => batchSize))
+      }
+    }
+  }
+
   override def getPartitions: Array[Partition] = {
     val url = EventsRelation.baseEventsURL(project).addQueryParameter("onlyCursors", "true").build()
-    val parts = CdpConnector.getWithCursor[EventItem](apiKey, url, batchSize, limit)
-      .filter(_.cursor.isDefined)
-      .map(chunk => chunk.cursor.get)
-    scala.util.Random.shuffle(parts)
+//    val parts = CdpConnector.getWithCursor[EventItem](apiKey, url, batchSize, limit)
+//      .filter(_.cursor.isDefined)
+//      .map(chunk => chunk.cursor.get)
+    scala.util.Random.shuffle(cursors(Uri.unsafeFromString(url.toString).withQueryParam("onlyCursors", "true")))
       .toIndexedSeq
       .zipWithIndex
-      .map { case (cursor, index) => CdpRddPartition(cursor, index) }
+      .map { case ((cursor, size), index) => CdpRddPartition(cursor, size, index) }
       .toArray
   }
 
   override def compute(_split: Partition, context: TaskContext): Iterator[Row] = {
     println("COMPUTE")
+    val url = EventsRelation.baseEventsURL(project)
     val split = _split.asInstanceOf[CdpRddPartition]
+    val getUrl = split.cursor.fold(url)(cursor => url.addQueryParameter("cursor", cursor))
     val cdpRows = CdpConnector.get[EventItem](apiKey,
-      EventsRelation.baseEventsURL(project).build(),
-      batchSize, Some(batchSize), 10, Some(split.cursor))
+      getUrl.build(),
+      batchSize, split.size, 10, split.cursor)
       .map(item => {
         Row(item.id, item.startTime, item.endTime, item.description, item.`type`, item.subtype,
           item.metadata, item.assetIds, item.source, item.sourceId)
