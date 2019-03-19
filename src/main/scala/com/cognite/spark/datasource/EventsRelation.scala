3e769c693ad91879fc04d4772e07e18233399308
Håkon Trømborg <hakon.tromborg@cognite.com>
2019-03-19 15:45:08 +0100

    Add filter pushdown support for events

--- src/main/scala/com/cognite/spark/datasource/EventsRelation.scala
+++ src/main/scala/com/cognite/spark/datasource/EventsRelation.scala
@@ -9,6 +9,7 @@ import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import io.circe.parser.decode
 import com.softwaremill.sttp._
 import SparkSchemaHelper._
+import org.apache.spark.rdd.RDD
 
 import scala.concurrent.ExecutionContext
 
@@ -84,8 +85,10 @@ case class EventConflict(duplicates: Seq[SourceWithResourceId])
 class EventsRelation(config: RelationConfig)(@transient val sqlContext: SQLContext)
     extends CdpRelation[EventItem](config, "events")
     with InsertableRelation
+    with PrunedFilteredScan
     with CdpConnector {
   @transient lazy private val eventsCreated = metricsSource.getOrCreateCounter(s"events.created")
+  @transient lazy private val eventsRead = metricsSource.getOrCreateCounter(s"events.read")
 
   override def insert(data: DataFrame, overwrite: Boolean): Unit =
     data.foreachPartition(rows => {
@@ -164,6 +167,66 @@ class EventsRelation(config: RelationConfig)(@transient val sqlContext: SQLConte
     (postUpdate, postNewItems).parMapN((_, _) => ())
   }
 
+  case class ColumnFilter(
+      column: String
+  )
+
+  // scalastyle:off cyclomatic.complexity
+  def getFilter(filter: Filter, colName: String): Seq[ColumnFilter] =
+    filter match {
+      case IsNotNull(`colName`) => Seq()
+      case EqualTo(`colName`, value) => Seq(ColumnFilter(value.toString))
+      case EqualNullSafe(`colName`, value) => Seq(ColumnFilter(value.toString))
+      case In(`colName`, values) => values.map(v => ColumnFilter(v.toString))
+      case And(f1, f2) => getFilter(f1, colName) ++ getFilter(f2, colName)
+      case Or(f1, f2) => getFilter(f1, colName) ++ getFilter(f2, colName)
+      // TODO: add support for String filtering using the 'Search for events' endpoint
+      case StringStartsWith(`colName`, value) =>
+        sys.error(
+          s"Filtering using 'string starts with' not allowed for events, attempted for ${value.toString}")
+      case StringEndsWith(`colName`, value) =>
+        sys.error(
+          s"Filtering using 'string ends with' not allowed for events, attempted for ${value.toString}")
+      case StringContains(`colName`, value) =>
+        sys.error(
+          s"Filtering using 'string contains' not allowed for data points, attempted for ${value.toString}")
+      case _ =>
+        Seq()
+    }
+
+  // scalastyle:on cyclomatic.complexity
+  override def buildScan(): RDD[Row] = buildScan(Array.empty, Array.empty)
+
+  override def buildScan(requiredColumns: Array[String], filters: Array[Filter]): RDD[Row] = {
+    val types = filters.flatMap(getFilter(_, "type")).map(_.column)
+    val subTypes = filters.flatMap(getFilter(_, "subtype")).map(_.column)
+
+    val params = Map("type" -> types.toString, "subtype" -> subTypes.toString)
+
+    val rdds = for {
+      eventType <- if (types.isEmpty) { Array(None) } else { types.map(Some(_)) }
+      eventSubType <- if (subTypes.isEmpty) { Array(None) } else { subTypes.map(Some(_)) }
+    } yield {
+      val urlWithType = eventType.fold(listUrl(config))(listUrl(config).param("type", _))
+      val urlWithTypeAndSubType = eventSubType.fold(urlWithType)(urlWithType.param("subtype", _))
+
+      CdpRdd[EventItem](
+        sqlContext.sparkContext,
+        (e: EventItem) => {
+          if (config.collectMetrics) {
+            eventsRead.inc()
+          }
+          toRow(e, requiredColumns)
+        },
+        urlWithTypeAndSubType.param("onlyCursors", "true"),
+        urlWithTypeAndSubType,
+        config
+      )
+    }
+
+    rdds.foldLeft(sqlContext.sparkContext.emptyRDD[Row])((a, b) => a.union(b))
+  }
+
   def baseEventsURL(project: String, version: String = "0.6"): Uri =
     uri"${config.baseUrl}/api/$version/projects/$project/events"
 
@@ -171,6 +234,12 @@ class EventsRelation(config: RelationConfig)(@transient val sqlContext: SQLConte
 
   override def toRow(t: EventItem): Row = asRow(t)
 
+  def toRow(t: EventItem, requiredColumns: Array[String]): Row = {
+    val values = t.productIterator
+    val eventMap = t.getClass.getDeclaredFields.map(_.getName -> values.next).toMap
+
+    Row.fromSeq(requiredColumns.map(eventMap(_)).toSeq)
+  }
   override def listUrl(relationConfig: RelationConfig): Uri =
     uri"${config.baseUrl}/api/0.6/projects/${config.project}/events"
 
