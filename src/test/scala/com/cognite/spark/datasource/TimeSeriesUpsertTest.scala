ec950895455debacc3b2875d0508955a3d2b52a4
Joel Wilsson <joel.wilsson@cognite.com>
2019-05-16 09:26:24 +0200

    Improve robustness of TimeSeriesUpsertTest

--- src/test/scala/com/cognite/spark/datasource/TimeSeriesUpsertTest.scala
+++ src/test/scala/com/cognite/spark/datasource/TimeSeriesUpsertTest.scala
@@ -22,14 +22,10 @@ class TimeSeriesUpsertTest extends FlatSpec with Matchers with SparkTest {
     val testUnit = "test data"
 
     // Clean up any old test data
-    val namesDf =
-      spark.sql(s"""select name from sourceTimeSeries where unit = '$testUnit'""")
-    val namesList = namesDf.rdd.map(r => r.getAs[String](0)).collect().toList
-    cleanupTimeSeries(namesList)
-
-    val testTimeSeriesAfterCleanup = retryWhile[Array[Row]](
-      spark.sql(s"""select * from sourceTimeSeries where unit = 'test data'""").collect,
-      df => df.length > 0)
+    val testTimeSeriesAfterCleanup = retryWhile[Array[Row]]({
+      cleanupTestData(testUnit)
+      spark.sql(s"""select * from sourceTimeSeries where unit = '$testUnit'""").collect
+    }, df => df.length > 0)
     assert(testTimeSeriesAfterCleanup.length == 0)
 
     // Insert new time series test data
@@ -39,7 +35,7 @@ class TimeSeriesUpsertTest extends FlatSpec with Matchers with SparkTest {
          |concat('TEST', name) as name,
          |isString,
          |metadata,
-         |'test data' as unit,
+         |'$testUnit' as unit,
          |'' as assetId,
          |isStep,
          |cast(array() as array<long>) as securityCategories,
@@ -59,37 +55,38 @@ class TimeSeriesUpsertTest extends FlatSpec with Matchers with SparkTest {
       df => df.length < 5)
     assert(initialDescriptionsAfterPost.length == 5)
 
-    // Upsert time series data
-    spark
-      .sql(s"""
-         |select $updatedDescription as description,
-         |name,
-         |isString,
-         |metadata,
-         |'test data' as unit,
-         |'' as assetId,
-         |isStep,
-         |securityCategories,
-         |id,
-         |null as createdTime,
-         |lastUpdatedTime
-         |from sourceTimeSeries
-         |where description = $initialDescription
+    val updatedDescriptionsAfterUpsert = retryWhile[Array[Row]]({
+      // Upsert time series data
+      spark
+        .sql(s"""
+                |select $updatedDescription as description,
+                |name,
+                |isString,
+                |metadata,
+                |'test data' as unit,
+                |'' as assetId,
+                |isStep,
+                |securityCategories,
+                |id,
+                |null as createdTime,
+                |lastUpdatedTime
+                |from sourceTimeSeries
+                |where description = $initialDescription
      """.stripMargin)
-      .select(sourceDf.columns.map(col): _*)
-      .write
-      .insertInto("sourceTimeSeries")
-
-    // Check if upsert worked
-    val updatedDescriptionsAfterUpsert = retryWhile[Array[Row]](
-      spark.sql(s"""select * from sourceTimeSeries where description = $updatedDescription""").collect,
-      df => df.length < 5)
+        .select(sourceDf.columns.map(col): _*)
+        .write
+        .insertInto("sourceTimeSeries")
+      // Check if upsert worked
+      spark.sql(s"""select * from sourceTimeSeries where description = $updatedDescription""").collect
+    }, df => df.length < 5)
     assert(updatedDescriptionsAfterUpsert.length == 5)
 
     val initialDescriptionsAfterUpsert = retryWhile[Array[Row]](
       spark.sql(s"""select * from sourceTimeSeries where description = $initialDescription""").collect,
       df => df.length > 0)
     assert(initialDescriptionsAfterUpsert.length == 0)
+
+    cleanupTestData(testUnit)
   }
 
   it should "support abort in savemode" taggedAs WriteTest in {
@@ -97,10 +94,11 @@ class TimeSeriesUpsertTest extends FlatSpec with Matchers with SparkTest {
     val saveModeUnit = "spark-savemode-test"
 
     // Clean up any old test data
-    val namesDf =
-      spark.sql(s"""select name from sourceTimeSeries where unit = '$saveModeUnit'""")
-    val namesList = namesDf.rdd.map(r => r.getAs[String](0)).collect().toList
-    cleanupTimeSeries(namesList)
+    val testTimeSeriesAfterCleanup = retryWhile[Array[Row]]({
+      cleanupTestData(saveModeUnit)
+      spark.sql(s"""select * from sourceTimeSeries where unit = '$saveModeUnit'""").collect
+    }, df => df.length > 0)
+    assert(testTimeSeriesAfterCleanup.length == 0)
 
     // Insert new time series test data
     spark.sql(
@@ -163,26 +161,25 @@ class TimeSeriesUpsertTest extends FlatSpec with Matchers with SparkTest {
   it should "support partial update in savemode" taggedAs WriteTest in {
     val updateDescription = "spark-update-test"
     val saveModeUnit = "spark-savemode-test"
-    // Update data with a new description
-    spark.sql(
-      s"""
-         |select '$updateDescription' as description,
-         |id,
-         |name
-         |from sourceTimeSeries
-         |where unit = '$saveModeUnit'
-     """.stripMargin)
-      .write
-      .format("com.cognite.spark.datasource")
-      .option("apiKey", writeApiKey.apiKey)
-      .option("type", "timeseries")
-      .option("onconflict", "update")
-      .save()
 
-    val dfWithDescriptionUpdateTest = retryWhile[Array[Row]](
-      spark.sql(s"select * from sourceTimeSeries where description = '$updateDescription'").collect,
-      df => df.length < 5
-    )
+    val dfWithDescriptionUpdateTest = retryWhile[Array[Row]]({
+      // Update data with a new description
+      spark.sql(
+        s"""
+           |select '$updateDescription' as description,
+           |id,
+           |name
+           |from sourceTimeSeries
+           |where unit = '$saveModeUnit'
+     """.stripMargin)
+        .write
+        .format("com.cognite.spark.datasource")
+        .option("apiKey", writeApiKey.apiKey)
+        .option("type", "timeseries")
+        .option("onconflict", "update")
+        .save()
+      spark.sql(s"select * from sourceTimeSeries where description = '$updateDescription'").collect
+    }, df => df.length < 5)
     assert(dfWithDescriptionUpdateTest.length == 5)
 
     // Trying to update nonexisting Time Series should throw a CdpApiException
@@ -197,7 +194,7 @@ class TimeSeriesUpsertTest extends FlatSpec with Matchers with SparkTest {
                    |assetId,
                    |isStep,
                    |securityCategories,
-                   |id+5 as id,
+                   |bigint(1) as id,
                    |createdTime,
                    |lastUpdatedTime
                    |from sourceTimeSeries
@@ -214,8 +211,8 @@ class TimeSeriesUpsertTest extends FlatSpec with Matchers with SparkTest {
     val updateCdpApiException = updateError.getCause.asInstanceOf[CdpApiException]
     assert(updateCdpApiException.code == 400)
     spark.sparkContext.setLogLevel("WARN")
-
   }
+
   it should "support upsert in savemode" taggedAs WriteTest in {
     val upsertDescription = "spark-upsert-test"
     val saveModeUnit = "spark-savemode-test"
@@ -269,16 +266,28 @@ class TimeSeriesUpsertTest extends FlatSpec with Matchers with SparkTest {
     )
     assert(dfWithDescriptionUpsertTest.length == 10)
 
+    cleanupTestData(saveModeUnit)
   }
-  def cleanupTimeSeries(names: List[String]): Unit = {
+
+  def cleanupTestData(testName: String): Unit = {
+    val namesDf =
+      spark.sql(s"""select name from sourceTimeSeries where unit = '$testName'""")
+    cleanupTimeSeries(namesDf.rdd.map(r => r.getAs[String](0)).collect())
+  }
+
+  def cleanupTimeSeries(names: Array[String]): Unit = {
     val project = getProject(writeApiKey, Constants.DefaultMaxRetries, Constants.DefaultBaseUrl)
 
     for (name <- names) {
-      delete(
-        writeApiKey.apiKey,
-        uri"${Constants.DefaultBaseUrl}/api/0.5/projects/$project/timeseries/$name",
-        maxRetries = 5
-      ).unsafeRunSync()
+      try {
+        delete(
+          writeApiKey.apiKey,
+          uri"${Constants.DefaultBaseUrl}/api/0.5/projects/$project/timeseries/$name",
+          maxRetries = 5
+        ).unsafeRunSync()
+      } catch {
+        case CdpApiException(_, 404, _) => // ignore exceptions about already deleted items
+      }
     }
   }
 }
