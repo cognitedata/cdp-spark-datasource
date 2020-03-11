a1c5fcf7be592d4e7fe6c3c8ce1c3d1792eda26b
Tapped <emilalexer@hotmail.com>
2020-03-11 15:34:42 +0100

    Use Summary instead of Histogram
    Histogram only works for fixed buckets, while we probably want streaming quantiles

--- performancebench/src/main/scala/cognite/spark/performancebench/Metrics.scala
+++ performancebench/src/main/scala/cognite/spark/performancebench/Metrics.scala
@@ -1,12 +1,12 @@
 package cognite.spark.performancebench
 
-import io.prometheus.client.Histogram
+import io.prometheus.client.Summary
 
 object Metrics {
-  val testTimeHistogram = Histogram
+  // We don't really know the buckets, so summary is a better metric type than histogram
+  val testTimeSummary = Summary
     .build()
     .name("cognite_cdf_spark_test_seconds")
-    .buckets(1, 5, 10, 20, 40, 50, 60)
     .labelNames("testName")
     .help("The number of seconds a benchmark test takes. The 'testName' label stores the test ran.")
     .register()
