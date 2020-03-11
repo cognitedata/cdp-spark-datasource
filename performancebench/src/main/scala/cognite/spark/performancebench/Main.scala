86e8b2f200852c85905ae9fa0757670620921a03
Tapped <emilalexer@hotmail.com>
2020-03-11 16:01:25 +0100

    Simplify main method

--- performancebench/src/main/scala/cognite/spark/performancebench/Main.scala
+++ performancebench/src/main/scala/cognite/spark/performancebench/Main.scala
@@ -1,12 +1,6 @@
 package cognite.spark.performancebench
 
-import cats.effect.{ExitCode, IO, IOApp}
-
-object Main extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = {
-    val eventPerf = new EventPerformance()
-    eventPerf.run()
-    sys.exit(0)
-    IO.pure(ExitCode.Success)
-  }
+object Main extends App {
+  val eventPerf = new EventPerformance()
+  eventPerf.run()
 }
