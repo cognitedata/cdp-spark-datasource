1415cc6d1fbf01b766a4b9ed67ea70770ab3a10e
Tapped <emilalexer@hotmail.com>
2020-03-11 16:10:16 +0100

    Share cogniteApiKey var

--- performancebench/src/main/scala/cognite/spark/performancebench/SparkUtil.scala
+++ performancebench/src/main/scala/cognite/spark/performancebench/SparkUtil.scala
@@ -4,20 +4,17 @@ import org.apache.spark.sql.{DataFrame, DataFrameReader, DataFrameWriter, Row, S
 
 trait SparkUtil {
   val spark = SparkUtil.spark
+  val cogniteApiKey = sys.env.getOrElse("COGNITE_API_KEY", throw new Exception("'COGNITE_API_KEY' is not set"))
 
   def read(): DataFrameReader =
     spark.read
       .format("cognite.spark.v1")
-      .option(
-        "apiKey",
-        sys.env.getOrElse("COGNITE_API_KEY", throw new Exception("'COGNITE_API_KEY' is not set")))
+      .option("apiKey", cogniteApiKey)
 
   def write(df: DataFrame): DataFrameWriter[Row] =
     df.write
       .format("cognite.spark.v1")
-      .option(
-        "apiKey",
-        sys.env.getOrElse("COGNITE_API_KEY", throw new Exception("'COGNITE_API_KEY' is not set")))
+      .option("apiKey", cogniteApiKey)
 }
 
 object SparkUtil {
