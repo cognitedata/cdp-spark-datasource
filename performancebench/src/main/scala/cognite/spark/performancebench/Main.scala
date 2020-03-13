6282e7c854dc70ee6f379f44c9566edd038d3d28
Tapped <emilalexer@hotmail.com>
2020-03-13 16:08:27 +0100

    Sleep before exiting to make sure metrics are scraped

--- performancebench/src/main/scala/cognite/spark/performancebench/Main.scala
+++ performancebench/src/main/scala/cognite/spark/performancebench/Main.scala
@@ -1,6 +1,17 @@
 package cognite.spark.performancebench
 
+import io.prometheus.client.exporter.HTTPServer
+import org.log4s._
+
 object Main extends App {
-  val eventPerf = new EventPerformance()
-  eventPerf.run()
+  val logger = getLogger
+  val metricsServer = new HTTPServer(8123, true)
+  try {
+    val eventPerf = new EventPerformance()
+    eventPerf.run()
+  } finally {
+    logger.info("Sleeping for 20 seconds before exiting, so that all metrics are scraped.")
+    Thread.sleep(20*1000) /* Sleep for 20 seconds to make sure metrics are scraped */
+    metricsServer.stop()
+  }
 }
