8a191280a01aa637a391fdaead26d15a2ee50770
Joel Wilsson <joel.wilsson@gmail.com>
2020-03-24 18:21:11 +0100

    Add a more complicated test for SdkV1Rdd.compute

--- src/test/scala/cognite/spark/v1/SdkV1RddTest.scala
+++ src/test/scala/cognite/spark/v1/SdkV1RddTest.scala
@@ -1,13 +1,17 @@
 package cognite.spark.v1
 
 import cats.effect.IO
+import cats.implicits._
 import com.cognite.sdk.scala.common.CdpApiException
-import com.cognite.sdk.scala.v1.GenericClient
-import fs2.Stream
+import com.cognite.sdk.scala.v1.{Event, GenericClient}
+import fs2.{Chunk, Stream}
 import org.apache.spark.TaskContext
 import org.apache.spark.sql.Row
 import org.scalatest.{FlatSpec, Matchers}
 import com.softwaremill.sttp._
+import cognite.spark.v1.SparkSchemaHelper.{asRow, fromRow, structType}
+import org.apache.spark.sql.types.{DataTypes, StructType}
+import scala.concurrent.duration._
 
 class SdkV1RddTest extends FlatSpec with Matchers with SparkTest {
   it should "throw an error when passed streams that return an error" in {
@@ -32,12 +36,40 @@ class SdkV1RddTest extends FlatSpec with Matchers with SparkTest {
     def toRow(s: String): Row = Row.empty
     def uniqueId(s: String): String = "1"
 
-    val sdkRdd = SdkV1Rdd(spark.sparkContext, getDefaultConfig(readApiKeyAuth), toRow, uniqueId, getStreams)
+    val sdkRdd =
+      SdkV1Rdd(spark.sparkContext, getDefaultConfig(readApiKeyAuth), toRow, uniqueId, getStreams)
 
     val e = intercept[CdpApiException] {
       sdkRdd.compute(CdfPartition(0), TaskContext.get())
     }
     assert(e.message == errorMessage)
     assert(e.code == 400)
+  }
+
+  it should "convert multiple streams to one Iterator" in {
+    import CdpConnector._
+
+    val nStreams = 50
+    val nItemsPerStream = 1000
+    val rdd = new SdkV1Rdd[Event, Long](
+      spark.sparkContext,
+      DefaultSource
+        .parseRelationConfig(Map("apiKey" -> writeApiKey), spark.sqlContext)
+        .copy(parallelismPerPartition = nStreams),
+      (e: Event) => asRow(e),
+      (e: Event) => e.id,
+      (_: GenericClient[IO, Nothing], _: Option[Int], _: Int) => {
+        val allStreams = 0.until(nStreams).map { i =>
+          Stream.evalUnChunk {
+            IO.sleep((scala.math.random() * 300).millis)(cdpConnectorTimer) *> IO(
+              Chunk.seq(1.to(nItemsPerStream).map(j => Event(id = i * nItemsPerStream + j))))
+          }
+        }
+        // Duplicates should be filtered out, so appending streams shouldn't make any difference.
+        allStreams ++ allStreams ++ allStreams
+      }
+    )
+
+    assert(rdd.compute(CdfPartition(0), TaskContext.get()).size == nStreams * nItemsPerStream)
   }
 }
