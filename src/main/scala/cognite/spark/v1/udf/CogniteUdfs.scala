16c4981c051f9cb4db3869f8dc2fcc6b7bb42169
ebrattli <eric@brattli.net>
2020-07-21 16:45:30 +0200

    Add functions API as UDF

---
+++ src/main/scala/cognite/spark/v1/udf/CogniteUdfs.scala
@@ -0,0 +1,139 @@
+package cognite.spark.v1.udf
+
+import cats.effect.IO
+import cognite.spark.v1.Constants.{DefaultBaseUrl, DefaultMaxRetries}
+import cognite.spark.v1.udf.CogniteUdfs.{
+  callFunctionAndGetResult,
+  callFunctionByExternalId,
+  callFunctionByName
+}
+import cognite.spark.v1.{CdfSparkException, CdpConnector, Constants}
+import com.cognite.sdk.scala.common.ApiKeyAuth
+import com.cognite.sdk.scala.v1.{FunctionCall, GenericClient}
+import com.softwaremill.sttp.SttpBackend
+import io.circe.{Json, JsonObject, parser}
+import org.apache.spark.sql.SparkSession
+
+class CogniteUdfs(sparkSession: SparkSession) {
+  def initializeUdfs(apiKey: ApiKeyAuth, baseUrl: String = DefaultBaseUrl): Unit = {
+    sparkSession.udf.register(
+      "cdf_function",
+      (functionId: Long, data: String) =>
+        callFunctionAndGetResult(
+          functionId,
+          data,
+          apiKey,
+          baseUrl
+        ).unsafeRunSync()
+    )
+    sparkSession.udf.register(
+      "cdf_function_external_id",
+      (externalId: String, data: String) =>
+        callFunctionByExternalId(
+          externalId,
+          data,
+          apiKey,
+          baseUrl
+        ).unsafeRunSync()
+    )
+    sparkSession.udf.register(
+      "cdf_function_name",
+      (name: String, data: String) =>
+        callFunctionByName(
+          name,
+          data,
+          apiKey,
+          baseUrl
+        ).unsafeRunSync()
+    )
+  }
+}
+
+object CogniteUdfs {
+  @transient implicit lazy val backend: SttpBackend[IO, Nothing] =
+    CdpConnector.retryingSttpBackend(DefaultMaxRetries)
+
+  private def getFunctionResult(
+      client: GenericClient[IO],
+      functionId: Long,
+      result: FunctionCall): IO[Json] = {
+    var res = result
+    var i = 0
+    while (res.status.isDefined && res.status.get == "Running" && i < 30) {
+      Thread.sleep(500)
+      i += 1
+      res = client
+        .functionCalls(functionId)
+        .retrieveById(res.id.get)
+        .unsafeRunSync()
+    }
+
+    if (res.status.isDefined && res.status.get == "Completed") {
+      for {
+        functionCallResponse <- client
+          .functionCalls(functionId)
+          .retrieveResponse(res.id.getOrElse(0L))
+      } yield functionCallResponse.response.getOrElse(Json.fromJsonObject(JsonObject.empty))
+    } else {
+      throw new CdfSparkException("CDF function call failed. Call = " + res.toString)
+    }
+  }
+
+  private def callFunctionAndGetResult(
+      functionId: Long,
+      data: String,
+      apiKeyAuth: ApiKeyAuth,
+      baseUrl: String): IO[String] = {
+    val jsonData = parser.parse(data) match {
+      case Right(value) => value
+      case Left(_) => Json.fromJsonObject(JsonObject.empty)
+    }
+
+    for {
+      client <- GenericClient.forAuth[IO](
+        Constants.SparkDatasourceVersion,
+        apiKeyAuth,
+        baseUrl,
+        apiVersion = Some("playground"))
+      call <- client
+        .functionCalls(functionId)
+        .callFunction(Json.fromJsonObject(JsonObject(("data", jsonData))))
+      res <- getFunctionResult(client, functionId, call)
+    } yield res.toString
+  }
+
+  private def callFunctionByExternalId(
+      externalId: String,
+      data: String,
+      apiKeyAuth: ApiKeyAuth,
+      baseUrl: String) = {
+    val funcIO = for {
+      client <- GenericClient.forAuth[IO](
+        Constants.SparkDatasourceVersion,
+        apiKeyAuth,
+        baseUrl,
+        apiVersion = Some("playground"))
+      func <- client.functions.retrieveByExternalId(externalId)
+    } yield func
+
+    val func = funcIO.unsafeRunSync()
+    callFunctionAndGetResult(func.id.get, data, apiKeyAuth, baseUrl)
+  }
+
+  private def callFunctionByName(name: String, data: String, apiKeyAuth: ApiKeyAuth, baseUrl: String) = {
+    val functionsIO = for {
+      client <- GenericClient.forAuth[IO](
+        Constants.SparkDatasourceVersion,
+        apiKeyAuth,
+        baseUrl,
+        apiVersion = Some("playground"))
+      functions <- client.functions.read()
+    } yield functions
+
+    val functionOption = functionsIO.unsafeRunSync().items.find(func => func.name == name)
+    functionOption match {
+      case Some(value) => callFunctionAndGetResult(value.id.get, data, apiKeyAuth, baseUrl)
+      case None => IO.pure("")
+    }
+  }
+}
