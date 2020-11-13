1bc0acab10ed036032c0305d17d0d8df27538574
Håkon Trømborg <hakon.tromborg@cognite.com>
2020-11-13 10:24:59 +0100

    Start logging stuff

--- src/main/scala/cognite/spark/v1/SdkV1Relation.scala
+++ src/main/scala/cognite/spark/v1/SdkV1Relation.scala
@@ -13,6 +13,7 @@ import io.scalaland.chimney.Transformer
 import io.scalaland.chimney.dsl._
 import CdpConnector._
 import io.circe.JsonObject
+import org.log4s._
 
 abstract class SdkV1Relation[A <: Product, I](config: RelationConfig, shortName: String)
     extends CdfRelation(config, shortName)
@@ -20,6 +21,7 @@ abstract class SdkV1Relation[A <: Product, I](config: RelationConfig, shortName:
     with TableScan
     with PrunedFilteredScan {
 
+  val logger = getLogger
   def schema: StructType
 
   def toRow(a: A): Row
@@ -132,9 +134,11 @@ abstract class SdkV1Relation[A <: Product, I](config: RelationConfig, shortName:
       resourceCreates: Seq[C],
       resource: T,
       doUpsert: Boolean)(implicit transform: Transformer[C, U]): IO[Unit] = {
+    logger.info("I'm in createOrUpdateByExternalId now")
     val (resourcesToUpdate, resourcesToCreate) = resourceCreates.partition(
       p => p.externalId.exists(id => existingExternalIds.contains(id))
     )
+    logger.info(s"resourceToCreate: ${resourcesToCreate.length}, resourcesToUpdate: ${resourcesToUpdate.length}")
     val create = if (resourcesToCreate.isEmpty) {
       IO.unit
     } else {
@@ -153,6 +157,7 @@ abstract class SdkV1Relation[A <: Product, I](config: RelationConfig, shortName:
                 assertNoLegacyNameConflicts(duplicated, requestId)
                 duplicated.flatMap(j => j("externalId")).map(_.asString.get)
             }
+            logger.info(s"Duplicates found: ${moreExistingExternalIds.length}")
             createOrUpdateByExternalId[R, U, C, T](
               existingExternalIds ++ moreExistingExternalIds.toSet,
               resourcesToCreate,
@@ -213,7 +218,7 @@ abstract class SdkV1Relation[A <: Product, I](config: RelationConfig, shortName:
         case (Some(_), items) => items.take(1)
       }
       .toSeq
-
+    logger.info(s"itemsToCreateWithoutDuplicatesByExternalId: ${itemsToCreateWithoutDuplicatesByExternalId.length}")
     val update = updateByIdOrExternalId[U, Up, Re, R](
       itemsToUpdate,
       resource,
