92c3ce6126e62477d792b3b18fb472d412fb0047
Joel Wilsson <joel.wilsson@gmail.com>
2021-02-04 11:48:19 +0100

    Interrupt streams and iterators when tasks are killed/completed

--- src/main/scala/cognite/spark/v1/SdkV1Rdd.scala
+++ src/main/scala/cognite/spark/v1/SdkV1Rdd.scala
@@ -1,15 +1,15 @@
 package cognite.spark.v1
 
 import java.util.concurrent.{ArrayBlockingQueue, ConcurrentHashMap, Executors}
-
 import cats.effect.IO
 import com.cognite.sdk.scala.common.Auth
 import com.cognite.sdk.scala.v1._
 import com.softwaremill.sttp.SttpBackend
+import fs2.concurrent.SignallingRef
 import fs2.{Chunk, Stream}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
-import org.apache.spark.{Partition, SparkContext, TaskContext}
+import org.apache.spark.{InterruptibleIterator, Partition, SparkContext, TaskContext}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -40,7 +40,20 @@ final case class SdkV1Rdd[A, I](
   override def compute(_split: Partition, context: TaskContext): Iterator[Row] = {
     val split = _split.asInstanceOf[CdfPartition]
 
+    // Spark doesn't cancel tasks immediately if using a normal
+    // iterator. Instead, they provide InterruptibleIterator,
+    // which does. We also need to interrupt our streams to stop
+    // reading data from CDF, which can continue for a while even
+    // after the iterator has stopped.
+    val shouldStop = SignallingRef[IO, Boolean](false).unsafeRunSync()
+    Option(context).foreach { ctx =>
+      ctx.addTaskCompletionListener[Unit] { _ =>
+        shouldStop.set(true).unsafeRunSync()
+      }
+    }
+
     val streams = getStreams(client, config.limitPerPartition, config.partitions)
+      .map(_.interruptWhen(shouldStop))
     val groupedStreams = streams.grouped(config.parallelismPerPartition).toSeq
     val currentStreamsAsSingleStream = groupedStreams(split.index).reduce(_.merge(_))
 
@@ -52,7 +65,14 @@ final case class SdkV1Rdd[A, I](
         Option(processedIds.putIfAbsent(uniqueId(i), ())).isEmpty
       }
     }
-    StreamIterator(currentStreamsAsSingleStream, config.parallelismPerPartition * 2, Some(processChunk))
-      .map(toRow)
+
+    val it = StreamIterator(
+      currentStreamsAsSingleStream,
+      config.parallelismPerPartition * 2,
+      Some(processChunk)).map(toRow)
+    Option(context) match {
+      case Some(ctx) => new InterruptibleIterator(ctx, it)
+      case None => it
+    }
   }
 }
