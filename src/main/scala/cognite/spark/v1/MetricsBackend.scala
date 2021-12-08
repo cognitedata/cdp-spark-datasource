6a78d0cea7f04a7a6b4455c98912a7cff268d4d6
Stanislav Lukeš <stanislav.lukes@cognite.com>
2021-12-08 21:22:17 +0000

    Fix metrics for responses with >= 400 status code

--- src/main/scala/cognite/spark/v1/MetricsBackend.scala
+++ src/main/scala/cognite/spark/v1/MetricsBackend.scala
@@ -3,6 +3,7 @@ package cognite.spark.v1
 import cats.effect.Sync
 import cats.syntax.all._
 import com.codahale.metrics.Counter
+import com.cognite.sdk.scala.common.{CdpApiException, SdkException}
 import sttp.capabilities.Effect
 import sttp.client3.{Request, Response, SttpBackend}
 import sttp.model.StatusCode
@@ -16,23 +17,29 @@ class MetricsBackend[F[_]: Sync, +S](
     responseCounter: StatusCode => Option[Counter] = _ => None,
     failureCounter: => Option[Counter] = None
 ) extends SttpBackend[F, S] {
+  private def incFailure() =
+    Sync[F].delay(
+      failureCounter.foreach(_.inc())
+    )
+  private def incResponse(code: StatusCode) =
+    Sync[F].delay(
+      responseCounter(code).foreach(_.inc())
+    )
+
   override def send[T, R >: S with Effect[F]](
       request: Request[T, R]
   ): F[Response[T]] =
     Sync[F].delay(metric.inc()) *> delegate
       .send(request)
-      .attempt
-      .flatTap {
-        case Right(response) =>
-          Sync[F].delay(
-            responseCounter(response.code).foreach(_.inc())
-          )
-        case Left(_) =>
-          Sync[F].delay(
-            failureCounter.foreach(_.inc())
-          )
+      .onError {
+        case cdpError: CdpApiException => incResponse(StatusCode(cdpError.code))
+        case SdkException(_, _, _, Some(code)) => incResponse(StatusCode(code))
+        case _ =>
+          incFailure()
       }
-      .rethrow
+      .flatTap { response =>
+        incResponse(response.code)
+      }
 
   override def close(): F[Unit] = delegate.close()
   override def responseMonad: MonadError[F] = delegate.responseMonad
