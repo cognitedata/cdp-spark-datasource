ff5775eaa262252907fe40da15c46479387f4a0e
Joel Wilsson <joel.wilsson@cognite.com>
2022-07-06 13:28:58 +0200

    Fix MetricsSource without asScala

--- src/main/scala/cognite/spark/v1/MetricsSource.scala
+++ src/main/scala/cognite/spark/v1/MetricsSource.scala
@@ -11,18 +11,16 @@ class MetricsSource {
   // Keeps track of all the Metric instances that are being published
   val metricsMap = new ConcurrentHashMap[String, Eval[Counter]]
 
-  def getOrElseUpdate(metricNamespace: String, metricName: String, metric: => Counter): Counter = {
-    val wrapped = Eval.later(metric)
-    metricsMap
-      .computeIfAbsent(s"${metricNamespace}.${metricName}", (_: String) => {
-        registerMetricSource(metricNamespace, metricName, wrapped.value)
-        wrapped
-      })
-      .value
+  def getOrCreateCounter(metricNamespace: String, metricName: String): Counter = {
+    val wrapped = Eval.later {
+      val counter = new Counter
+      registerMetricSource(metricNamespace, metricName, counter)
+      counter
+    }
+    val key = s"$metricNamespace.$metricName"
+    metricsMap.putIfAbsent(key, wrapped)
+    metricsMap.get(key).value
   }
-
-  def getOrCreateCounter(metricNamespace: String, metricName: String): Counter =
-    getOrElseUpdate(metricNamespace, metricName, new Counter)
 
   /**
     * Register a [[Metric]] with Spark's [[org.apache.spark.metrics.MetricsSystem]].
