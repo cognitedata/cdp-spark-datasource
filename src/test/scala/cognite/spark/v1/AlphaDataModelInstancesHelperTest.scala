51d1b49f19875edfd52eae1cf056259d6caee333
vuhainguyen <vu.nguyen@cognite.com>
2022-09-14 17:29:23 +0200

    Add test for class AlphaDataModelInstancesHelper

---
+++ src/test/scala/cognite/spark/v1/AlphaDataModelInstancesHelperTest.scala
@@ -0,0 +1,31 @@
+package cognite.spark.v1
+
+import cognite.spark.v1.AlphaDataModelInstancesHelper.getDirectRelationIdentifierProperty
+import com.cognite.sdk.scala.v1.DirectRelationIdentifier
+import org.apache.spark.sql.Row
+import org.scalatest.{FlatSpec, Matchers}
+
+class AlphaDataModelInstancesHelperTest extends FlatSpec with Matchers {
+  it should "use first colon as the separator for spaceExternalId and externalId when get Direct Relation Identifier" in {
+    val row = Row.fromSeq(
+      Seq(
+        "test-space1:externalId1",
+        "test-space2:externalId2:withColon2",
+        "test-space3:externalId3:withColon3a:withColon3b::withColon3c:::withColon3d"))
+    val dri1 = getDirectRelationIdentifierProperty(row, "", 0)
+    dri1 shouldBe DirectRelationIdentifier(Some("test-space1"), "externalId1")
+
+    val dri2 = getDirectRelationIdentifierProperty(row, "", 1)
+    dri2 shouldBe DirectRelationIdentifier(Some("test-space2"), "externalId2:withColon2")
+
+    val dri3 = getDirectRelationIdentifierProperty(row, "", 2)
+    dri3 shouldBe DirectRelationIdentifier(
+      Some("test-space3"),
+      "externalId3:withColon3a:withColon3b::withColon3c:::withColon3d")
+  }
+
+  it should "throw CdfSparkException if there is no colon in the row of Direct Relation Identifier" in {
+    val row = Row.fromSeq(Seq("test-space1externalId1"))
+    a[CdfSparkException] should be thrownBy getDirectRelationIdentifierProperty(row, "", 0)
+  }
+}
