7ab26d2317b39f43b5fa7ea2c862770f30cbea85
Vitaly Belekhov <vitaly.belekhov@cognite.com>
2022-12-06 10:59:32 +0100

    WellsRelation based on DataModelInstanceRelation.

--- src/main/scala/cognite/spark/v1/WellsRelation.scala
+++ src/main/scala/cognite/spark/v1/WellsRelation.scala
@@ -9,124 +9,153 @@ import com.cognite.sdk.scala.v1.resources.Wells
 import fs2.Stream
 import io.scalaland.chimney.Transformer
 import io.scalaland.chimney.dsl._
-import org.apache.spark.sql.sources.{Filter, InsertableRelation}
+import org.apache.spark.sql.sources.{Filter, InsertableRelation, TableScan}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{Row, SQLContext}
 
 import java.time.Instant
 
-class WellsRelation(config: RelationConfig, subtreeIds: Option[List[CogniteId]] = None)(
-    sqlContext: SQLContext)
-    extends SdkV1Relation[WellsReadSchema, Long](config, "assets")
-    with InsertableRelation
-    with WritableRelation {
-  Array("name", "source", "dataSetId", "labels", "id", "externalId", "externalIdPrefix")
-  override def getStreams(sparkFilters: Array[Filter])(
-      client: GenericClient[IO],
-      limit: Option[Int],
-      numPartitions: Int): Seq[Stream[IO, WellsReadSchema]] = {
-    (ids, filters) =
-      pushdownToFilters(sparkFilters, assetsFilterFromMap, WellsFilter(assetSubtreeIds = subtreeIds))
-    executeFilter(client.assets, filters, ids, numPartitions, limit)
-      .map(
-        _.map(
-          _.into[WellsReadSchema]
-            .withFieldComputed(_.labels, asset => cogniteExternalIdSeqToStringSeq(asset.labels))
-            .withFieldComputed(
-              _.aggregates,
-              asset =>
-                asset.aggregates.map { aggregates =>
-                  WellsAggregatesSchema(
-                    aggregates.get("childCount"),
-                    None, // No support for path yet.
-                    aggregates.get("depth")
-                  )
-              }
-            )
-            .transform))
-  }
-
-  private def assetsFilterFromMap(m: Map[String, String]): WellsFilter =
-    WellsFilter(
-      name = m.get("name"),
-      source = m.get("source"),
-      dataSetIds = m.get("dataSetId").map(idsFromStringifiedArray(_).map(CogniteInternalId(_))),
-      labels = m.get("labels").flatMap(externalIdsToContainsAny),
-      lastUpdatedTime = timeRange(m, "lastUpdatedTime"),
-      createdTime = timeRange(m, "createdTime"),
-      externalIdPrefix = m.get("externalIdPrefix"),
-      assetSubtreeIds = subtreeIds
-    )
-
-  override def insert(rows: Seq[Row]): IO[Unit] = {
-    assetsInsertions = rows.map(fromRow[WellIngestionInsertSchema](_))
-    assets = assetsInsertions.map(
-      _.into[WellCreate]
-        .withFieldComputed(_.labels, u => stringSeqToCogniteExternalIdSeq(u.labels))
-        .transform)
-    client.assets
-      .create(assets)
-      .flatTap(_ => incMetrics(itemsCreated, assets.size)) *> IO.unit
-  }
-
-  private def isUpdateEmpty(u: WellUpdate): Boolean = u == WellUpdate()
-
-  override def update(rows: Seq[Row]): IO[Unit] = {
-    assetUpdates = rows.map(r => fromRow[WellIngestionInsertSchema](r))
-
-    updateByIdOrExternalId[WellIngestionInsertSchema, WellUpdate, Wells[IO], Well](
-      assetUpdates,
-      client.assets,
-      isUpdateEmpty
-    )
-  }
-
-  override def delete(rows: Seq[Row]): IO[Unit] = {
-    deletes = rows.map(fromRow[DeleteItemByCogniteId](_))
-    deleteWithIgnoreUnknownIds(client.assets, deletes.map(_.toCogniteId), config.ignoreUnknownIds)
-  }
-
-  override def upsert(rows: Seq[Row]): IO[Unit] = {
-    assets = rows.map(fromRow[WellIngestionInsertSchema](_))
-    genericUpsert[Well, WellIngestionInsertSchema, WellCreate, WellUpdate, Wells[IO]](
-      assets,
-      isUpdateEmpty,
-      client.assets,
-      mustBeUpdate = r => r.name.isEmpty && r.getExternalId.nonEmpty
-    )
-  }
-
-  override def getFromRowsAndCreate(rows: Seq[Row], doUpsert: Boolean = true): IO[Unit] = {
-    assetsUpserts = rows.map(fromRow[WellIngestionInsertSchema](_))
-    assets = assetsUpserts.map(_.transformInto[WellCreate])
-    createOrUpdateByExternalId[Well, WellUpdate, WellCreate, WellCreate, Option, Wells[IO]](
-      Set.empty,
-      assets,
-      client.assets,
-      doUpsert = true)
-  }
+class WellsRelation(
+    config: RelationConfig)(override val sqlContext: SQLContext)
+  extends CdfRelation(config, "wdl")
+    with WritableRelation
+    with TableScan {
 
   override def schema: StructType = structType[WellsReadSchema]()
 
-  override def toRow(a: WellsReadSchema): Row = asRow(a)
+  override def upsert(rows: Seq[Row]): IO[Unit] =
+    if (rows.isEmpty) {
+      IO.unit
+    } else {
+      val insertions = rows.map(fromRow[WellIngestionInsertSchema](_))
 
-  override def uniqueId(a: WellsReadSchema): Long = a.id
+      val wells = insertions.map(
+        _.into[AssetCreate]
+          .withFieldComputed(_.labels, u => stringSeqToCogniteExternalIdSeq(u.labels))
+          .transform)
+    }
+
+  override def insert(rows: Seq[Row]): IO[Unit] =
+    throw new CdfSparkException(
+      "Create (abort) is not supported for data model instances. Use upsert instead.")
+
+  def toRow(a: ProjectedDataModelInstance): Row = {
+    if (config.collectMetrics) {
+      itemsRead.inc()
+    }
+    new GenericRow(a.properties)
+  }
+
+  def uniqueId(a: ProjectedDataModelInstance): String = a.externalId
+
+  def getStreams(filters: Array[Filter], selectedColumns: Array[String])(
+    @nowarn client: GenericClient[IO],
+    limit: Option[Int],
+    @nowarn numPartitions: Int): Seq[Stream[IO, ProjectedDataModelInstance]] = {
+    val selectedPropsArray: Array[String] = if (selectedColumns.isEmpty) {
+      schema.fields.map(_.name)
+    } else {
+      selectedColumns
+    }
+
+    val filter: DomainSpecificLanguageFilter = {
+      val andFilters = filters.toVector.flatMap(getInstanceFilter)
+      if (andFilters.isEmpty) EmptyFilter else DSLAndFilter(andFilters)
+    }
+
+    val dmiQuery = DataModelInstanceQuery(
+      model = DataModelIdentifier(space = Some(spaceExternalId), model = modelExternalId),
+      spaceExternalId = instanceSpaceExternalIdFilter,
+      filter = filter,
+      sort = None,
+      limit = limit,
+      cursor = None
+    )
+
+      Seq(
+        alphaClient.nodes
+          .queryStream(dmiQuery, limit)
+          .map(r => toProjectedInstance(r, selectedPropsArray)))
+  }
+
+  override def buildScan(selectedColumns: Array[String], filters: Array[Filter]): RDD[Row] =
+    SdkV1Rdd[ProjectedDataModelInstance, String](
+      sqlContext.sparkContext,
+      config,
+      (item: ProjectedDataModelInstance, _) => toRow(item),
+      uniqueId,
+      getStreams(filters, selectedColumns)
+    )
+
+  override def delete(rows: Seq[Row]): IO[Unit] = {
+    val deletes = rows.map(r => SparkSchemaHelper.fromRow[DataModelInstanceDeleteSchema](r))
+      alphaClient.nodes
+        .deleteItems(deletes.map(_.externalId), spaceExternalId)
+        .flatTap(_ => incMetrics(itemsDeleted, rows.length))
+  }
+
+  override def update(rows: Seq[Row]): IO[Unit] =
+    throw new CdfSparkException("Update is not supported for data model instances. Use upsert instead.")
+
+  def getIndexedPropertyList(rSchema: StructType): Array[(Int, String, DataModelPropertyDefinition)] =
+    rSchema.fields.zipWithIndex.map {
+      case (field: StructField, index: Int) =>
+        val propertyType = modelInfo.getOrElse(
+          field.name,
+          throw new CdfSparkException(
+            s"Can't insert property `${field.name}` " +
+              s"into data model $modelExternalId, the property does not exist in the definition")
+        )
+        (index, field.name, propertyType)
+    }
+
+  def wellFromRow(schema: StructType): Row => Node = {
+
+    val externalIdIndex = getRequiredStringPropertyIndex(schema, modelType, "externalId")
+    val indexedPropertyList: Array[(Int, String, DataModelPropertyDefinition)] = getIndexedPropertyList(
+      schema)
+
+    def parseWellRow(indexedPropertyList: Array[(Int, String, DataModelPropertyDefinition)])(
+      row: Row): Node = {
+      val externalId = getStringValueForFixedProperty(row, "externalId", externalIdIndex)
+      val propertyValues: Map[String, DataModelProperty[_]] =
+        getDataModelPropertyMap(indexedPropertyList, row)
+
+      Node(externalId, Some(propertyValues))
+    }
+
+    parseNodeRow(indexedPropertyList)
+  }
+
+  def getOptionalIndex(
+    rSchema: StructType,
+    keyString: String): Option[Int] = {
+      val index = rSchema.fieldNames.indexOf(keyString)
+      if (index < 0) {
+        None
+      } else {
+        Some(index)
+      }
+  }
+
+  def getRequiredIndex(
+    rSchema: StructType,
+    keyString: String): Int = {
+      getOptionalIndex(rSchema, keyString).getOrElse(
+        throw new CdfSparkException(s"Can't row convert to Well, `$keyString` is missing.")
+      )
+  }
+
 }
 
-object WellsRelation extends UpsertSchema {
-  upsertSchema: StructType = structType[WellIngestionInsertSchema]()
-  insertSchema: StructType = structType[WellIngestionInsertSchema]()
-  readSchema: StructType = structType[WellsReadSchema]()
-}
-
-final case class WellheadsReadSchema(
+final case class Wellheads(
     x: Double,
     y: Double,
     /* The coordinate reference systems of the wellhead. */
     crs: String,
 )
 
-final case class AssetSourceReadSchema(
+final case class AssetSource(
     /* Asset external ID. */
     assetExternalId: String,
     /* Name of the source this asset external ID belongs to. */
@@ -159,7 +188,7 @@ final case class Datum (
     reference: String,
 )
 
-final case class WellboreReadSchema (
+final case class Wellbore (
     /* Unique identifier used to match wellbores from different sources. The matchingId must be unique within a source. */
     matchingId: String,
     /* Name of the wellbore. */
@@ -167,7 +196,7 @@ final case class WellboreReadSchema (
     /* Matching id of the associated well. */
     wellMatchingId: String,
     /* List of sources that are associated to this wellbore. */
-    sources: Seq[AssetSourceReadSchema],
+    sources: Seq[AssetSource],
     /* Description of the wellbore. */
     description: Option[String] = None,
     /* Parent wellbore if it exists. */
@@ -184,9 +213,9 @@ final case class WellsReadSchema(
     matchingId: String,
     /* Name of the well. */
     name: String,
-    wellhead: WellheadsReadSchema,
+    wellhead: Wellheads,
     /* List of sources that are associated to this well. */
-    sources: Seq[AssetSourceReadSchema],
+    sources: Seq[AssetSource],
     /* Description of the well. */
     description: Option[String] = None,
     /* Also called UWI. */
@@ -212,14 +241,14 @@ final case class WellsReadSchema(
     /* Water depth of the well. Vertical distance from the mean sea level (MSL) to the sea bed. */
     waterDepth: Option[Distance] = None,
     /* List of wellbores that are associated to this well. */
-    wellbores: Option[Seq[WellboreReadSchema]] = None,
+    wellbores: Option[Seq[Wellbore]] = None,
 )
 
 final case class WellIngestionInsertSchema (
     /* Name of the well. */
     name: String,
     /* Connection between this well and the well asset with a given source. */
-    source: AssetSourceReadSchema,
+    source: AssetSource,
     /* Unique identifier used to match wells from different sources. The matchingId must be unique within a source. */
     matchingId: Option[String] = None,
     /* Description of the well. */
@@ -246,12 +275,5 @@ final case class WellIngestionInsertSchema (
     license: Option[String] = None,
     /* Water depth of the well. Vertical distance from the mean sea level (MSL) to the sea bed. */
     waterDepth: Option[Distance] = None,
-    wellhead: Option[WellheadsReadSchema] = None,
-)
-
-final case class WellsAggregatesSchema(
-    // TODO: add actual support for these aggregated properties
-    childCount: Option[Long] = None,
-    path: Option[Array[String]] = None,
-    depth: Option[Long] = None
+    wellhead: Option[Wellheads] = None,
 )
