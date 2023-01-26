014348b8716c4b3a49f2da165b24df27faddfdbb
Vitaly Belekhov <vitaly.belekhov@cognite.com>
2023-01-26 17:00:00 +0100

    Cleanup and refactoring of trajectories tests, new trajectory test added.

--- src/test/scala/cognite/spark/v1/wdl/WDLTrajectoriesTest.scala
+++ src/test/scala/cognite/spark/v1/wdl/WDLTrajectoriesTest.scala
@@ -25,54 +25,50 @@ class WDLTrajectoriesTest
     client.miniSetup()
   }
 
+  val rawTrajectoryRowsDF: Unit = spark.read
+    .json("src/test/resources/wdl-test-raw-trajectory-rows.jsonl")
+    .createOrReplaceTempView("wdl_test_raw_trajectory_rows")
+
+  val rawTrajectoriesDF: Unit = spark.read
+    .json("src/test/resources/wdl-test-raw-trajectories.jsonl")
+    .createOrReplaceTempView("wdl_test_raw_trajectories")
+
+  val getTrajectoryRowsCTE: String =
+    """with r as (
+      |  select wellboreAssetExternalId,
+      |    collect_list(
+      |      struct(
+      |        azimuth,
+      |        inclination,
+      |        measuredDepth,
+      |        doglegSeverity
+      |      )
+      |    ) as rows
+      |   from wdl_test_raw_trajectory_rows
+      |   group by wellboreAssetExternalId
+      | )""".stripMargin
+
+  val getTrajectoriesExpr: String =
+    """select
+      |   t.wellboreAssetExternalId,
+      |   struct(t.sourceName, t.externalId as sequenceExternalId) as source,
+      |   t.type,
+      |   t.measuredDepthUnit,
+      |   t.inclinationUnit,
+      |   t.azimuthUnit,
+      |   t.doglegSeverityUnit,
+      |   t.phase,
+      |   r.rows as rows
+      | from wdl_test_raw_trajectories as t
+      | left join r on r.wellboreAssetExternalId = t.wellboreAssetExternalId
+      |""".stripMargin
+
   it should "ingest and read bent trajectory" in {
-    val testTrajectoryIngestionRowsDF = spark.read
-      .json("src/test/resources/wdl-test-trajectory-rows.jsonl")
-    testTrajectoryIngestionRowsDF.createOrReplaceTempView("wdl_test_trajectory_rows")
-
-    val testTrajectoryIngestionsDF = spark.read
-      .schema(client.getSchema("TrajectoryIngestion"))
-      .json("src/test/resources/wdl-test-trajectories.jsonl")
-    testTrajectoryIngestionsDF.createOrReplaceTempView("wdl_test_trajectories")
-
-    testTrajectoryIngestionsDF.printSchema()
-
     val testIngestionsDF = spark
-      .sql(s"""with r as (
-           |  select wellboreAssetExternalId,
-           |    collect_list(
-           |      named_struct(
-           |        "azimuth", azimuth,
-           |        "inclination", inclination,
-           |        "measuredDepth", measuredDepth,
-           |        "doglegSeverity", doglegSeverity
-           |      )
-           |    ) as rows
-           |   from wdl_test_trajectory_rows
-           |   group by wellboreAssetExternalId
-           | )
-           | select
-           |   t.wellboreAssetExternalId,
-           |   t.source,
-           |   t.type,
-           |   t.measuredDepthUnit,
-           |   t.inclinationUnit,
-           |   t.azimuthUnit,
-           |   t.doglegSeverityUnit,
-           |   t.phase,
-           |   r.rows as rows
-           | from wdl_test_trajectories as t
-           | left join r on r.wellboreAssetExternalId = t.wellboreAssetExternalId""".stripMargin)
-
-//      .sql(
-//        s"""with r as
-//           | (select wellboreAssetExternalId, named_struct(measuredDepth, inclination, azimuth from wdl_test_trajectory_rows) as row)
-//           | select *, to_json(collect_list(r.row)) as rows
-//           | from wdl_test_trajectories
-//           | join r on wellboreAssetExternalId""".stripMargin)
-
-    testIngestionsDF.printSchema()
-    testIngestionsDF.show(truncate = false)
+      .sql(s"""$getTrajectoryRowsCTE
+           | $getTrajectoriesExpr
+           |""".stripMargin)
+      .filter("""wellboreAssetExternalId = "A:wb1"""")
 
     testIngestionsDF.write
       .format("cognite.spark.v1")
@@ -82,14 +78,42 @@ class WDLTrajectoriesTest
       .useOIDCWrite
       .save()
 
-    val testTrajectoriesDF = spark.read
+    val expectedTrajectoriesDF = spark.read
       .schema(client.getSchema("Trajectory"))
       .json("src/test/resources/wdl-test-expected-trajectories.jsonl")
+      .filter("""wellboreAssetExternalId = "A:wb1"""")
 
-    val TrajectoriesDF = sparkReader
+    val trajectoriesDF = sparkReader
       .option("wdlDataType", "Trajectory")
       .load()
 
-    (testTrajectoriesDF.collect() should contain).theSameElementsAs(TrajectoriesDF.collect())
+    (expectedTrajectoriesDF.collect() should contain).theSameElementsAs(trajectoriesDF.collect())
+  }
+
+  it should "ingest and read trajectory in feet" in {
+    val testIngestionsDF = spark
+      .sql(s"""$getTrajectoryRowsCTE
+           | $getTrajectoriesExpr
+           |""".stripMargin)
+      .filter("""wellboreAssetExternalId = "A:wb2"""")
+
+    testIngestionsDF.write
+      .format("cognite.spark.v1")
+      .option("project", "jetfiretest2")
+      .option("type", "welldatalayer")
+      .option("wdlDataType", "TrajectoryIngestion")
+      .useOIDCWrite
+      .save()
+
+    val expectedTrajectoriesDF = spark.read
+      .schema(client.getSchema("Trajectory"))
+      .json("src/test/resources/wdl-test-expected-trajectories.jsonl")
+      .filter("""wellboreAssetExternalId = "A:wb2"""")
+
+    val trajectoriesDF = sparkReader
+      .option("wdlDataType", "Trajectory")
+      .load()
+
+    (expectedTrajectoriesDF.collect() should contain).theSameElementsAs(trajectoriesDF.collect())
   }
 }
