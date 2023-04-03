e73556ffd7c6131118b298e39d6f89e978732336
Tharindu Dhananjaya Galappaththi <tdgalappaththi@gmail.com>
2023-04-03 13:56:14 +0200

    Auto create dependent instances

--- src/main/scala/cognite/spark/v1/FlexibleDataModelCorePropertyRelation.scala
+++ src/main/scala/cognite/spark/v1/FlexibleDataModelCorePropertyRelation.scala
@@ -274,7 +274,11 @@ private[spark] class FlexibleDataModelCorePropertyRelation(
       case Right(items) if items.nonEmpty =>
         val instanceCreate = InstanceCreate(
           items = items,
-          replace = Some(true)
+          replace = Some(true),
+          // These options need to made dynamic by moving to frontend
+          // https://cognitedata.slack.com/archives/C03G11UNHBJ/p1678971213050319
+          autoCreateStartNodes = Some(true),
+          autoCreateEndNodes = Some(true)
         )
         client.instances.createItems(instanceCreate)
       case Right(_) => IO.pure(Vector.empty)
