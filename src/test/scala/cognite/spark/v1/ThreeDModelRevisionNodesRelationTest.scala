48b9ba8ae362d0a054b3831d79141c5588bd26d3
Dmitry Ivankov <dmitry.ivankov@cognite.com>
2023-09-28 11:37:39 +0200

    Update src/test/scala/cognite/spark/v1/ThreeDModelRevisionNodesRelationTest.scala

--- src/test/scala/cognite/spark/v1/ThreeDModelRevisionNodesRelationTest.scala
+++ src/test/scala/cognite/spark/v1/ThreeDModelRevisionNodesRelationTest.scala
@@ -21,6 +21,7 @@ class ThreeDModelRevisionNodesRelationTest extends FlatSpec with ParallelTestExe
 
   "ThreeDModelRevisionNodesRelationTest" should "pass a smoke test" taggedAs WriteTest in {
     val (model, revision) = getSomeModelRevision.unsafeRunSync()
+    assert("" == s"${model} => ${revision}")
 
     val df = spark.read
       .format(DefaultSource.sparkFormatString)
