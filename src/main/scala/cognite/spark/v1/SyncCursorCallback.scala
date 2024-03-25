9220e6133f3f22e78c7afe6237ff3a86af189f73
Thorkild Stray <thorkild.stray@cognite.com>
2024-03-25 11:23:55 +0100

    Remove the now unneeded IORuntime configuration

--- src/main/scala/cognite/spark/v1/SyncCursorCallback.scala
+++ src/main/scala/cognite/spark/v1/SyncCursorCallback.scala
@@ -1,57 +1,21 @@
 package cognite.spark.v1
 
 import cats.effect.IO
-import cats.effect.unsafe.{IORuntime, IORuntimeConfig}
 import com.cognite.sdk.scala.sttp.GzipBackend
-import com.google.common.util.concurrent.ThreadFactoryBuilder
 import io.circe.Codec
 import io.circe.generic.semiauto.deriveCodec
-import org.log4s.getLogger
 import sttp.client3.{SttpBackend, basicRequest}
 import sttp.client3.asynchttpclient.SttpClientBackendFactory
 import sttp.client3.asynchttpclient.cats.AsyncHttpClientCatsBackend
 import sttp.client3.circe.{asJson, circeBodySerializer}
 import sttp.model.{MediaType, Uri}
 
-import java.lang.Thread.UncaughtExceptionHandler
-import java.util.concurrent.Executors
-import scala.concurrent.ExecutionContext
-
 case class IncrementalCursor(name: String, value: String)
 case class IncrementalCursorResponse(name: String, value: String, updated: Boolean)
 
 object SyncCursorCallback {
-  @transient private val logger = getLogger
   @transient private lazy val sttpBackend: SttpBackend[IO, Any] =
     new GzipBackend[IO, Any](AsyncHttpClientCatsBackend.usingClient(SttpClientBackendFactory.create()))
-
-  @transient private lazy val syncCursorCallbackExecutionContext: ExecutionContext =
-    ExecutionContext.fromExecutor(
-      Executors.newFixedThreadPool(
-        2,
-        new ThreadFactoryBuilder()
-          .setDaemon(true)
-          .setUncaughtExceptionHandler(new UncaughtExceptionHandler {
-            override def uncaughtException(t: Thread, e: Throwable): Unit =
-              logger.warn(e)(
-                s"Ignoring uncaught exception when submitting cursor callback: ${e.getMessage}")
-          })
-          .setNameFormat("Cursor-Callback-%d")
-          .build()
-      )
-    )
-
-  @transient private lazy val (blocking, _) =
-    IORuntime.createDefaultBlockingExecutionContext("Cursor-Callback-blocking")
-  @transient private lazy val (scheduler, _) =
-    IORuntime.createDefaultScheduler("Cursor-Callback-scheduler")
-  @transient lazy implicit val ioRuntime: IORuntime = IORuntime(
-    syncCursorCallbackExecutionContext,
-    blocking,
-    scheduler,
-    () => (),
-    IORuntimeConfig()
-  )
 
   implicit val incrementalCursorCodec: Codec[IncrementalCursor] = deriveCodec[IncrementalCursor]
   implicit val incrementalCursorResponseCodec: Codec[IncrementalCursorResponse] =
