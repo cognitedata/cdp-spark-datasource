00177fab8a183ed1e5612037d71414b65d4c0eb5
Andreas Korneliussen <andreas.korneliussen@cognite.com>
2024-03-27 13:49:33 +0100

    Resolve connection before sync

--- src/main/scala/cognite/spark/v1/DefaultSource.scala
+++ src/main/scala/cognite/spark/v1/DefaultSource.scala
@@ -71,8 +71,8 @@ class DefaultSource
 
     corePropertySyncRelation
       .orElse(corePropertyRelation)
-      .orElse(datamodelBasedSync)
       .orElse(dataModelBasedConnectionRelation)
+      .orElse(datamodelBasedSync)
       .orElse(dataModelBasedCorePropertyRelation)
       .orElse(connectionRelation)
       .getOrElse(throw new CdfSparkException(
