b754a7aba3a428ac0ec95a30ba197c718749b911
Jacob Eliat-Eliat <jacob.eliat-eliat@cognite.com>
2024-07-08 10:47:10 +0200

    Revert "running scalafmt too"
    This reverts commit 697f4702c26fc3a202a8619d4127cbe744928690.

--- src/test/scala/cognite/spark/v1/FilesRelationTest.scala
+++ src/test/scala/cognite/spark/v1/FilesRelationTest.scala
@@ -26,50 +26,41 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
         (destinationDf, targetView, metricsPrefix)
       }
     } {
-      case (_, targetView, _) =>
-        IO.blocking {
-          spark
-            .sql(s"""select * from ${targetView} where source = '${testSource}'""")
-            .write
-            .format(DefaultSource.sparkFormatString)
-            .useOIDCWrite
-            .option("type", "files")
-            .option("onconflict", "delete")
-            .save()
-        }
+      case (_, targetView, _) => IO.blocking {
+        spark
+          .sql(s"""select * from ${targetView} where source = '${testSource}'""")
+          .write
+          .format(DefaultSource.sparkFormatString)
+          .useOIDCWrite
+          .option("type", "files")
+          .option("onconflict", "delete")
+          .save()
+      }
     }
   }
 
   private def runIOTest[A](test: Resource[IO, A]): Unit =
     test.use(_ => IO.unit).unsafeRunSync()(cats.effect.unsafe.implicits.global)
 
-  private def makeLabels(externalIds: Seq[String]): Resource[IO, Seq[Label]] =
+  private def makeLabels(externalIds: Seq[String]): Resource[IO, Seq[Label]] = {
     Resource.make {
-      writeClient.labels.create(
-        externalIds.map(
-          externalId =>
-            LabelCreate(
-              description = Some("cdp-spark-connector FilesRelationTest"),
-              name = "for tests",
-              externalId = externalId,
-              dataSetId = None)))
-    } { _ =>
-      writeClient.labels.deleteByExternalIds(externalIds)
-    }
+      writeClient.labels.create(externalIds.map(externalId => LabelCreate(
+        description = Some("cdp-spark-connector FilesRelationTest"),
+        name = "for tests",
+        externalId = externalId,
+        dataSetId = None)))
+    } { _ => writeClient.labels.deleteByExternalIds(externalIds) }
+  }
 
   private def makeFiles(toCreate: Seq[FileCreate]): Resource[IO, Seq[File]] = {
     assert(toCreate.forall(_.source.isEmpty), "file.source is reserved for test setup")
-    Resource
-      .make {
-        writeClient.files.create(toCreate.map(_.copy(source = Some(testSource))))
-      } { createdFiles =>
-        writeClient.files.deleteByIds(createdFiles.map(_.id))
-      }
-      .evalTap(createdFiles =>
-        IO.delay({
-          assert(createdFiles.length == toCreate.length, "all files should be created")
-          assert(createdFiles.forall(_.source.contains(testSource)), "all created files have source")
-        }))
+    Resource.make {
+      writeClient.files.create(toCreate.map(_.copy(source = Some(testSource))))
+    } { createdFiles => writeClient.files.deleteByIds(createdFiles.map(_.id)) }
+      .evalTap(createdFiles => IO.delay({
+        assert(createdFiles.length == toCreate.length, "all files should be created")
+        assert(createdFiles.forall(_.source.contains(testSource)), "all created files have source")
+      }))
   }
 
   private def getDfReader(): DataFrameReader =
@@ -78,6 +69,7 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
       .option("type", "files")
       .useOIDCWrite
 
+
   "FilesRelation" should "read files" taggedAs ReadTest in {
     runIOTest(for {
       _ <- makeFiles(Seq.fill(4)(FileCreate(name = "file")))
@@ -85,7 +77,7 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
       sourceDf = getDfReader().load()
       _ = sourceDf.createOrReplaceTempView(sourceView)
       res <- Resource.eval(retryWhileIO[Array[Row]](IO.blocking {
-        spark.sqlContext
+         spark.sqlContext
           .sql(s"select * from ${sourceView} where source = '${testSource}'")
           .collect()
       }, _.length < 4))
@@ -96,21 +88,17 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
   it should "respect the limit option" taggedAs ReadTest in {
     runIOTest(for {
       _ <- makeFiles(Seq.fill(10)(FileCreate(name = "file")))
-      res <- Resource.eval(
-        retryWhileIO[Array[Row]](
-          IO.blocking {
-            val df = getDfReader()
-              .option("limitPerPartition", "5")
-              .option("partitions", "1")
-              .load()
-            val view = s"files_${shortRandomString()}"
-            df.createTempView(view)
-            spark.sqlContext
-              .sql(s"select * from ${view} where source = '$testSource'")
-              .collect()
-          },
-          _.length < 5
-        ))
+      res <- Resource.eval(retryWhileIO[Array[Row]](IO.blocking {
+        val df = getDfReader()
+          .option("limitPerPartition", "5")
+          .option("partitions", "1")
+          .load()
+        val view = s"files_${shortRandomString()}"
+        df.createTempView(view)
+        spark.sqlContext
+          .sql(s"select * from ${view} where source = '$testSource'")
+          .collect()
+      }, _.length < 5))
       _ = assert(res.length == 5)
     } yield ())
   }
@@ -118,20 +106,16 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
   it should "use cursors when necessary" taggedAs ReadTest in {
     runIOTest(for {
       _ <- makeFiles(Seq.fill(10)(FileCreate(name = "file")))
-      res <- Resource.eval(
-        retryWhileIO[Array[Row]](
-          IO.blocking {
-            val df = getDfReader()
-              .option("batchSize", "2")
-              .load()
-            val view = s"files_${shortRandomString()}"
-            df.createTempView(view)
-            spark.sqlContext
-              .sql(s"select * from ${view} where source = '$testSource'")
-              .collect()
-          },
-          _.length < 10
-        ))
+      res <- Resource.eval(retryWhileIO[Array[Row]](IO.blocking {
+        val df = getDfReader()
+          .option("batchSize", "2")
+          .load()
+        val view = s"files_${shortRandomString()}"
+        df.createTempView(view)
+        spark.sqlContext
+          .sql(s"select * from ${view} where source = '$testSource'")
+          .collect()
+      }, _.length < 10))
       _ = assert(res.length == 10)
     } yield ())
   }
@@ -144,7 +128,8 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
         .option("type", "files")
         .load()
       _ = spark
-        .sql(s"""
+        .sql(
+          s"""
              |select "name-$testSource" as name,
              |null as id,
              |null as directory,
@@ -170,7 +155,7 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
       rows = retryWhile[Array[Row]](
         spark.sql(s"select * from $targetView where source = '$testSource'").collect(),
         rows => rows.length < 1)
-      _ = assert(rows.length == 1)
+      _ = assert (rows.length == 1)
     } yield ())
   }
 
@@ -178,7 +163,8 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
     runIOTest(for {
       (_, targetView, metricsPrefix) <- makeDestinationDf()
       _ = spark
-        .sql(s"""
+        .sql(
+          s"""
              |select "name-$testSource" as name,
              |null as id,
              |'$testSource' as source,
@@ -194,55 +180,57 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
       rows = retryWhile[Array[Row]](
         spark.sql(s"""select * from ${targetView} where source = "$testSource"""").collect(),
         rows => rows.length < 1)
-      _ = assert(rows.length == 1)
+      _ = assert (rows.length == 1)
       _ = assert(getNumberOfRowsCreated(metricsPrefix, "files") == 1)
       id <- Resource.eval(writeClient.files.retrieveByExternalId(s"externalId-$testSource")).map(_.id)
       //Update using id
       _ = spark
-        .sql(s"""
+    .sql(s"""
             |select ${id.toString} as id,
             |'$testSource' as source,
             |'updatedById-externalId-$testSource' as externalId
       """.stripMargin)
-        .write
-        .format(DefaultSource.sparkFormatString)
-        .useOIDCWrite
-        .option("type", "files")
-        .option("onconflict", "update")
-        .option("collectMetrics", "true")
-        .option("metricsPrefix", metricsPrefix)
-        .save()
-      _ = assert(getNumberOfRowsCreated(metricsPrefix, "files") == 1)
-      _ = assert(getNumberOfRowsUpdated(metricsPrefix, "files") == 1)
-      updatedById = retryWhile[Array[Row]](
-        spark
-          .sql(s"select * from ${targetView} where externalId = 'updatedById-externalId-$testSource'")
-          .collect(),
-        df => df.length < 1)
-      _ = assert(updatedById.length == 1)
+          .write
+            .format(DefaultSource.sparkFormatString)
+          .useOIDCWrite
+            .option("type", "files")
+          .option("onconflict", "update")
+          .option("collectMetrics", "true")
+          .option("metricsPrefix", metricsPrefix)
+          .save()
+      _ = assert (getNumberOfRowsCreated(metricsPrefix, "files") == 1)
+      _ = assert (getNumberOfRowsUpdated(metricsPrefix, "files") == 1)
+      updatedById =
+        retryWhile[Array[Row]](
+          spark
+            .sql(s"select * from ${targetView} where externalId = 'updatedById-externalId-$testSource'")
+            .collect(),
+          df => df.length < 1)
+      _ = assert (updatedById.length == 1)
 
       //Update using externalId
       _ = spark
-        .sql(s"""
+            .sql(s"""
                     |select 'updatedById-externalId-$testSource' as externalId,
                     |'updatedByExternalId-$testSource' as source
         """.stripMargin)
-        .write
-        .format(DefaultSource.sparkFormatString)
-        .useOIDCWrite
-        .option("type", "files")
-        .option("onconflict", "update")
-        .option("collectMetrics", "true")
-        .option("metricsPrefix", metricsPrefix)
-        .save()
-      _ = assert(getNumberOfRowsCreated(metricsPrefix, "files") == 1)
-      _ = assert(getNumberOfRowsUpdated(metricsPrefix, "files") == 2)
-      updatedByExternalId = retryWhile[Array[Row]](
-        spark
-          .sql(s"select * from ${targetView} where source = 'updatedByExternalId-$testSource'")
-          .collect(),
-        df => df.length < 1)
-      _ = assert(updatedByExternalId.length == 1)
+            .write
+              .format(DefaultSource.sparkFormatString)
+            .useOIDCWrite
+              .option("type", "files")
+            .option("onconflict", "update")
+            .option("collectMetrics", "true")
+            .option("metricsPrefix", metricsPrefix)
+            .save()
+      _ = assert (getNumberOfRowsCreated(metricsPrefix, "files") == 1)
+      _ = assert (getNumberOfRowsUpdated(metricsPrefix, "files") == 2)
+      updatedByExternalId =
+        retryWhile[Array[Row]](
+          spark
+            .sql(s"select * from ${targetView} where source = 'updatedByExternalId-$testSource'")
+            .collect(),
+          df => df.length < 1)
+      _ = assert (updatedByExternalId.length == 1)
     } yield ())
   }
 
@@ -251,30 +239,32 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
       (_, targetView, _) <- makeDestinationDf()
       //insert data
       _ = spark
-        .sql(s"""
+          .sql(s"""
                   |select "upsert-example" as name,
                   |'$testSource' as source,
                   |'externalId-$testSource' as externalId,
                   |null as id,
                   |null as mimeType
       """.stripMargin)
-        .write
-        .format(DefaultSource.sparkFormatString)
-        .useOIDCWrite
-        .option("type", "files")
-        .option("onconflict", "upsert")
-        .save()
+          .write
+            .format(DefaultSource.sparkFormatString)
+          .useOIDCWrite
+            .option("type", "files")
+          .option("onconflict", "upsert")
+          .save()
       id <- Resource.eval(writeClient.files.retrieveByExternalId(s"externalId-$testSource")).map(_.id)
-      insertWithUpsertIds = retryWhile[Array[Row]](
-        spark
-          .sql(s"select * from ${targetView} where source = '$testSource'")
-          .collect(),
-        df => df.length < 1)
+      insertWithUpsertIds =
+        retryWhile[Array[Row]](
+          spark
+            .sql(s"select * from ${targetView} where source = '$testSource'")
+            .collect(),
+          df => df.length < 1)
       _ = assert(insertWithUpsertIds.length == 1)
 
       //update data
       _ = spark
-        .sql(s"""
+        .sql(
+          s"""
              |select ${id.toString} as id,
              |'text/plain-$testSource' as mimeType,
              |'upserted-$testSource' as source
@@ -287,13 +277,14 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
         .save()
 
       //check updated data
-      updatedWithUpsert = retryWhile[Array[Row]](
-        spark
-          .sql(
-            s"select * from ${targetView} where mimeType = 'text/plain-$testSource' and source = 'upserted-$testSource'")
-          .collect(),
-        df => df.length < 1)
-      _ = assert(updatedWithUpsert.length == 1)
+      updatedWithUpsert =
+        retryWhile[Array[Row]](
+          spark
+            .sql(
+              s"select * from ${targetView} where mimeType = 'text/plain-$testSource' and source = 'upserted-$testSource'")
+            .collect(),
+          df => df.length < 1)
+      _ = assert (updatedWithUpsert.length == 1)
 
       //original data doesn't exist
       updated = retryWhile[Array[Row]](
@@ -301,7 +292,7 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
           .sql(s"select * from ${targetView} where source = '$testSource'")
           .collect(),
         df => df.length > 0)
-      _ = assert(updated.isEmpty)
+      _ = assert (updated.isEmpty)
     } yield ())
   }
 
@@ -316,31 +307,32 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
                 |'externalId-$testSource' as externalId
     """.stripMargin)
         .write
-        .format(DefaultSource.sparkFormatString)
+          .format(DefaultSource.sparkFormatString)
         .useOIDCWrite
-        .option("type", "files")
+          .option("type", "files")
         .save()
 
       rows = retryWhile[Array[Row]](
         spark.sql(s"select id from ${targetView} where source = '$testSource'").collect(),
         rows => rows.length < 1)
-      _ = assert(rows.length == 1)
+      _ = assert (rows.length == 1)
 
       //Delete using id
       _ = spark
         .sql(s"select ${rows.head.getLong(0)} as id")
         .write
-        .format(DefaultSource.sparkFormatString)
+          .format(DefaultSource.sparkFormatString)
         .useOIDCWrite
-        .option("type", "files")
+          .option("type", "files")
         .option("onconflict", "delete")
         .save()
 
-      idsAfterDelete = retryWhile[Array[Row]](
-        spark
-          .sql(s"select id from ${targetView} where source = '$testSource'")
-          .collect(),
-        df => df.length > 0)
+      idsAfterDelete =
+          retryWhile[Array[Row]] (
+            spark
+            .sql(s"select id from ${targetView} where source = '$testSource'")
+        .collect(),
+          df => df.length > 0)
       _ = assert(idsAfterDelete.isEmpty)
     } yield ())
   }
@@ -365,12 +357,13 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
                 |'${testSource}' as source,
                 |array('test-label-${testSource}') as labels""".stripMargin)
         .write
-        .format(DefaultSource.sparkFormatString)
+          .format(DefaultSource.sparkFormatString)
         .option("type", "files")
         .useOIDCWrite
-        .save()
+          .save()
       res1 = retryWhile[Array[Row]](
-        spark.sql(s"""select * from ${targetView}
+        spark.sql(
+          s"""select * from ${targetView}
              |where labels = array('test-label-${testSource}')
              |and source='${testSource}'""".stripMargin).collect(),
         df => df.length != 1
@@ -378,7 +371,8 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
       _ = res1.length shouldBe 1
 
       res2 = retryWhile[Array[Row]](
-        spark.sql(s"""select * from ${targetView}
+        spark.sql(
+          s"""select * from ${targetView}
              |where labels in(array('test-label-${testSource}'), NULL)
              |and source='${testSource}'""".stripMargin).collect(),
         df => df.length != 1
@@ -386,7 +380,8 @@ class FilesRelationTest extends FlatSpec with Matchers with ParallelTestExecutio
       _ = res2.length shouldBe 1
 
       res3 = retryWhile[Array[Row]](
-        spark.sql(s"""select * from ${targetView}
+        spark.sql(
+          s"""select * from ${targetView}
              |where labels in(array('nonExistingLabel'), NULL)
              |and source='${testSource}'""".stripMargin).collect(),
         df => df.length != 0
--- src/test/scala/cognite/spark/v1/ThreeDModelRevisionNodesRelationTest.scala
+++ src/test/scala/cognite/spark/v1/ThreeDModelRevisionNodesRelationTest.scala
@@ -6,20 +6,18 @@ import com.cognite.sdk.scala.v1.{ThreeDModel, ThreeDRevision}
 import org.scalatest.{FlatSpec, ParallelTestExecution}
 
 class ThreeDModelRevisionNodesRelationTest extends FlatSpec with ParallelTestExecution with SparkTest {
-  private def getSomeModelRevision: IO[(ThreeDModel, ThreeDRevision)] =
-    writeClient.threeDModels
-      .list()
-      .flatMap(
-        model =>
-          writeClient
-            .threeDRevisions(model.id)
-            .list()
-            .filter(_.status == "Done")
-            .map((model, _)))
+  private def getSomeModelRevision: IO[(ThreeDModel, ThreeDRevision)] = {
+    writeClient.threeDModels.list()
+      .flatMap(model =>
+        writeClient.threeDRevisions(model.id).list()
+          .filter(_.status == "Done")
+          .map((model, _))
+      )
       .head
       .compile
       .toList
       .map(_.head)
+  }
 
   "ThreeDModelRevisionNodesRelationTest" should "pass a smoke test" taggedAs WriteTest in {
     val (model, revision) = getSomeModelRevision.unsafeRunSync()
