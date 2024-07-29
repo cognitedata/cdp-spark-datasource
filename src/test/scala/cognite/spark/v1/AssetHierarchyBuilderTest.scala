b216c507defa00ba9fb669a2070eb19d2b50699a
Dmitry Ivankov <dmitry.ivankov@cognite.com>
2024-07-29 10:01:07 +0200

    meh

--- src/test/scala/cognite/spark/v1/AssetHierarchyBuilderTest.scala
+++ src/test/scala/cognite/spark/v1/AssetHierarchyBuilderTest.scala
@@ -9,7 +9,8 @@ import org.scalatest.{FlatSpec, Matchers, OptionValues, ParallelTestExecution}
 
 import scala.annotation.nowarn
 
-@nowarn("cat=unused,unused-nowarn")
+@nowarn("cat=unused")
+@nowarn("cat=unused-nowarn")
 class AssetHierarchyBuilderTest
     extends FlatSpec
     with Matchers
--- src/test/scala/cognite/spark/v1/AssetsRelationTest.scala
+++ src/test/scala/cognite/spark/v1/AssetsRelationTest.scala
@@ -13,7 +13,8 @@ import java.util.UUID
 import scala.annotation.nowarn
 import scala.util.control.NonFatal
 
-@nowarn("cat=unused,unused-nowarn")
+@nowarn("cat=unused")
+@nowarn("cat=unused-nowarn")
 class AssetsRelationTest extends FlatSpec with Matchers with SparkTest {
 
   val sourceDf = dataFrameReaderUsingOidc
