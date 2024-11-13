bdfa2d14f0852d37326cc796e73312725ce883c4
Dmitry Ivankov <dmitry.ivankov@cognite.com>
2024-11-14 00:00:49 +0100

    add more comments

--- src/main/scala/cognite/spark/v1/RelationSchemas.scala
+++ src/main/scala/cognite/spark/v1/RelationSchemas.scala
@@ -3,6 +3,21 @@ package cognite.spark.v1
 import cognite.spark.compiletime.macros.SparkSchemaHelper.structType
 import org.apache.spark.sql.types.StructType
 
+/**
+  * Schema traits here aren't strict or currently compile-time checked, but they should
+  * indicate a schema compatible with what a relation expects or produces
+  *
+  * ReadSchema: for read operations
+  *
+  * DeleteSchema: for deletes
+  *
+  * {Insert,Upsert,Update}Schema: for writes, differ by conflict resolution mode
+  * - upsert: fully overwrites destination items if they exist already
+  * - insert: should fail if item already exists in destination. This is fragile as operation
+  *           retries on network errors may be doomed to fail
+  * - update: partially overwrites existing destination items, keeps properties that aren't part
+  *           of update schema (or from update row) intact
+  */
 trait InsertSchema {
   val insertSchema: StructType
 }
@@ -19,6 +34,7 @@ trait UpdateSchemaFromUpsertSchema extends UpsertSchema with UpdateSchema {
   override val updateSchema: StructType = upsertSchema
 }
 
+// TODO: this isn't applied to some relations that have read support
 trait ReadSchema {
   val readSchema: StructType
 }
