d7bb9eba4cea5164b2205e7b4a3ea1e63950ab83
Marcelo Lima <97023211+mr-celo@users.noreply.github.com>
2025-02-06 20:49:40 +0100

    Handle connection error on cursor update
    We used to only handle non-OK responses from `jetfire-backend`, meaning
    that connection errors such as timeouts or connection refused would
    throw and be unhandled

--- src/main/scala/cognite/spark/v1/SyncCursorCallback.scala
+++ src/main/scala/cognite/spark/v1/SyncCursorCallback.scala
@@ -5,13 +5,13 @@ import com.cognite.sdk.scala.sttp.{GzipBackend, RetryingBackend}
 import io.circe.Codec
 import io.circe.generic.semiauto.deriveCodec
 import org.log4s.getLogger
-
-import scala.concurrent.duration._
-import sttp.client3.{SttpBackend, basicRequest}
 import sttp.client3.asynchttpclient.SttpClientBackendFactory
 import sttp.client3.asynchttpclient.cats.AsyncHttpClientCatsBackend
 import sttp.client3.circe.{asJson, circeBodySerializer}
+import sttp.client3.{SttpBackend, basicRequest}
 import sttp.model.{MediaType, Uri}
+
+import scala.concurrent.duration._
 
 case class IncrementalCursor(name: String, value: String)
 case class IncrementalCursorResponse(name: String, value: String, updated: Boolean)
@@ -37,7 +37,25 @@ object SyncCursorCallback {
       callbackUrl: String,
       cursorName: String,
       cursorValue: String,
-      jobId: String): IO[IncrementalCursorResponse] = {
+      jobId: String
+  ): IO[IncrementalCursorResponse] =
+    sendCursorCallbackRequest(callbackUrl, cursorName, cursorValue, jobId, retryingBackend)
+      .map({
+        // We do not fail the whole transformation on a non-submitted cursor, as it is
+        // not critical for this job. Multiple failed runs so that the customer exceeds
+        // its max age, will lead to a fallback to restart.
+        case Left(error) =>
+          logger.warn(s"Failed to submit cursor $cursorName for jobId $jobId: ${error.getMessage}")
+          IncrementalCursorResponse(cursorName, cursorValue, updated = false)
+        case Right(value) => value
+      })
+
+  private[v1] def sendCursorCallbackRequest(
+      callbackUrl: String,
+      cursorName: String,
+      cursorValue: String,
+      jobId: String,
+      backend: SttpBackend[IO, Any]): IO[Either[Throwable, IncrementalCursorResponse]] = {
     val uri = Uri.parse(s"$callbackUrl/$jobId") match {
       case Left(value) => throw new IllegalArgumentException(s"Failed to parse URI '$value'")
       case Right(value) => value
@@ -50,16 +68,9 @@ object SyncCursorCallback {
       .body(IncrementalCursor(cursorName, cursorValue))
       .post(uri)
       .response(asJson[IncrementalCursorResponse])
-      .mapResponse {
-        // We do not fail the whole transformation on a non-submitted cursor, as it is
-        // not critical for this job. Multiple failed runs so that the customer exceeds
-        // its max age, will lead to a fallback to restart.
-        case Left(error) =>
-          logger.warn(s"Failed to submit cursor $cursorName for jobId $jobId: ${error.getMessage}")
-          IncrementalCursorResponse(cursorName, cursorValue, updated = false)
-        case Right(value) => value
-      }
-      .send(retryingBackend)
+      .send(backend)
       .map(_.body)
+      .attempt
+      .map(_.flatMap(identity))
   }
 }
